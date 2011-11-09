4d5f87b0805323578949c03ee8add7a8e4be5cc1
Mohammad Balkhi <balkhi.mohammad@hp.com>
2011-11-09 08:04:44 -0500

    addedon update latest code
    ----------------------------------- StarTeam -----------------------------------
    A smcfs/dev/PCAExtensions/plugins/com.xpedx.sterling.rcp.pca/com/xpedx/sterling/rcp/pca/customerprofilerule/screen/DivisionEntitlementPanel.java:1.0
    A smcfs/dev/PCAExtensions/plugins/com.xpedx.sterling.rcp.pca/com/xpedx/sterling/rcp/pca/customerprofilerule/screen/DivisionEntitlementPanelBehavior.java:1.0
    A smcfs/dev/PCAExtensions/plugins/com.xpedx.sterling.rcp.pca/com/xpedx/sterling/rcp/pca/referenceorder/action/XPXViewInvoicesUIAction.java:1.0
    A smcfs/dev/PCAExtensions/plugins/com.xpedx.sterling.rcp.pca/com/xpedx/sterling/rcp/pca/util/XPXSortListener.java:1.0
    --------------------------------------------------------------------------------

---
+++ smcfs/dev/PCAExtensions/plugins/com.xpedx.sterling.rcp.pca/com/xpedx/sterling/rcp/pca/referenceorder/action/XPXViewInvoicesUIAction.java
@@ -0,0 +1,62 @@
+package com.xpedx.sterling.rcp.pca.referenceorder.action;
+
+import java.net.URLEncoder;
+import org.eclipse.jface.action.IAction;
+import org.w3c.dom.Element;
+import com.xpedx.sterling.rcp.pca.util.TripleDES;
+import com.xpedx.sterling.rcp.pca.util.XPXUtils;
+import com.yantra.yfc.rcp.YRCEditorInput;
+import com.yantra.yfc.rcp.YRCPlatformUI;
+import com.yantra.yfc.rcp.YRCRelatedTask;
+import com.yantra.yfc.rcp.YRCRelatedTaskAction;
+
+
+public class XPXViewInvoicesUIAction extends YRCRelatedTaskAction {
+
+	private String finalQueryString;
+	String encryptedUserKey = null;
+	String URLEncodedUserKey = null;
+	
+	
+
+
+	@Override
+	public void executeTask(IAction arg0, YRCEditorInput arg1,
+			YRCRelatedTask arg2) {
+		// TODO Auto-generated method stub
+		    
+			YRCPlatformUI.showInformation("Message", "Click OK to continue");
+			String url = YRCPlatformUI.getYfsSystemProperty("xpedx.invoicing.url");  
+			Element ele = YRCPlatformUI.getUserElement();
+			String userKey= ele.getAttribute("UserKey");
+			TripleDES tripleDes = new TripleDES();
+			
+			
+			try {
+				encryptedUserKey = tripleDes.encrypt(userKey);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            URLEncodedUserKey = URLEncoder.encode(encryptedUserKey);
+            finalQueryString = "UserID="+ URLEncodedUserKey;
+			XPXUtils.accessURL(url, finalQueryString);
+			return;
+		
+	}
+	@Override
+	protected boolean checkForErrors() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	protected boolean checkForModifications() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+	
+	
+}
+
