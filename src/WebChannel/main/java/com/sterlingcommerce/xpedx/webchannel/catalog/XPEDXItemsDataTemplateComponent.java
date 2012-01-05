fea3ed58401668bfb6d9a23e358f6c9e314d1a1e
Vignesh Govindan <govindan.vignesh@hp.com>
2012-01-04 22:19:57 -0500

    Performance Optimization based on YSlow recommendation, fix for 3301, 2414, 1447
    ----------------------------------- StarTeam -----------------------------------
    A src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/catalog/XPEDXItemsDataTemplateComponent.java:1.0
    A src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/catalog/XPEDXItemsDataTemplateTag.java:1.0
    --------------------------------------------------------------------------------

---
+++ src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/catalog/XPEDXItemsDataTemplateComponent.java
@@ -0,0 +1,274 @@
+/**
+ * 
+ */
+package com.sterlingcommerce.xpedx.webchannel.catalog;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.components.Component;
+import org.w3c.dom.Element;
+
+import com.opensymphony.xwork2.util.TextUtils;
+import com.opensymphony.xwork2.util.ValueStack;
+import com.sterlingcommerce.webchannel.compat.SCXmlUtils;
+import com.sterlingcommerce.xpedx.webchannel.MyItems.utils.XPEDXMyItemsUtils;
+import com.sterlingcommerce.xpedx.webchannel.common.XPEDXSCXmlUtils;
+import com.sterlingcommerce.xpedx.webchannel.order.XPEDXOrderUtils;
+import com.sterlingcommerce.xpedx.webchannel.utilities.XPEDXUtilBean;
+
+/**
+ * @author vgovindan
+ * This class takes care of building JS objects with the items data for the templates in Catalog search results pages.
+ */
+public class XPEDXItemsDataTemplateComponent extends Component {
+
+	
+	public XPEDXItemsDataTemplateComponent(ValueStack stack, HttpServletRequest req,
+            HttpServletResponse res, XPEDXItemsDataTemplateTag tag) {
+		super(stack);
+		this.tag = tag;
+		this.req = req;
+	}
+
+	@Override
+	public boolean end(Writer writer, String body) {
+		
+		StringBuffer sb = new StringBuffer();
+		
+		Element item = (Element) findValue(tag.getItemElement(), Element.class);
+		int tabidx = 101;		
+		SCXmlUtils xmlUtils = XPEDXSCXmlUtils.getInstance();
+		Element info = xmlUtils.getChildElement(item, "PrimaryInformation");
+		String itemID = validate(item.getAttribute("ItemID"));
+		HashMap<String, String> itemUOMList = tag.getItemUomHashMap().get(itemID);
+		itemUOMList = itemUOMList == null ? new HashMap<String, String>() : itemUOMList;
+		String defaultUOM = validate(tag.getDefaultShowUOMMap().get(itemID));	
+		String shortDesc = validate(info.getAttribute("ShortDescription"));
+		String desc = validate(info.getAttribute("Description"));
+		String itemKey = validate(item.getAttribute("ItemKey"));
+		String kitCode = validate(info.getAttribute("KitCode"));
+		String unitOfMeasure = validate(item.getAttribute("UnitOfMeasure"));
+		Element price = xmlUtils.getChildElement(item, "ComputedPrice");
+		String myPrice = "";
+		if("BUNDLE".equals(kitCode))
+			myPrice = validate(price.getAttribute("BundleTotal"));
+		else 
+			myPrice = validate(price.getAttribute("UnitPrice"));
+		String pImg = (String)findValue("pImg");
+		String imageMainURL = validate(xmlUtils.getAttribute(info,"ImageLocation")) + "/" + validate(info.getAttribute("ImageID"));
+		if(!"/".equals(imageMainURL)) {
+			if (imageMainURL.startsWith("/"))
+				pImg = pImg.substring(0, pImg.indexOf("/", 1)) + imageMainURL;
+			else
+				pImg = pImg.substring(0, pImg.indexOf("/", 1)) + "/" + imageMainURL;
+		}
+		
+		
+		
+		HashMap<String, String> skuMap = tag.getItemMap().get("ItemID");
+		skuMap = skuMap == null ? new HashMap<String, String>() : skuMap;
+		XPEDXItemBranchInfoBean itemBranchBean = tag.getItemToItemBranchBeanMap().get("ItemID");
+		itemBranchBean = itemBranchBean == null ? new XPEDXItemBranchInfoBean("","","","","","","","") : itemBranchBean;
+		String orderMultiple = validate(tag.getOrderMultipleMap().get(itemID));		
+		Element b2cItemExtn = xmlUtils.getChildElement(item, "Extn");
+		String b2cSize = validate(b2cItemExtn.getAttribute("ExtnSize"));
+		String b2cColor = validate(b2cItemExtn.getAttribute("ExtnColor"));
+		String b2cMwt = validate(b2cItemExtn.getAttribute("ExtnMwt"));
+		String b2cBasis = validate(b2cItemExtn.getAttribute("ExtnBasis"));
+		String b2cCert = validate(b2cItemExtn.getAttribute("ExtnCert"));
+		String b2cVendorNumber = validate(b2cItemExtn.getAttribute("ExtnVendorNo"));
+		//String b2cLegacyId = validate(b2cItemExtn.getAttribute("ExtnLegacyId"));
+		String b2cMaterial = validate(b2cItemExtn.getAttribute("ExtnMaterial"));
+		String b2cForm = validate(b2cItemExtn.getAttribute("ExtnForm"));
+		String b2cCapacity = validate(b2cItemExtn.getAttribute("ExtnCapacity"));
+		String b2cModel = validate(b2cItemExtn.getAttribute("ExtnModel"));
+		//String b2cGuage = validate(b2cItemExtn.getAttribute("ExtnGauge"));
+		String b2cPly = validate(b2cItemExtn.getAttribute("ExtnPly"));
+		String b2cstockStatus = validate(itemBranchBean.getInventoryIndicator());
+		//String isSuperseded = validate(item.getAttribute("IsItemSuperseded"));
+		//String isValid = validate(info.getAttribute("IsValid"));
+		//String isModelItem = validate(info.getAttribute("IsModelItem"));
+		//String isConfigurable = validate(info.getAttribute("IsConfigurable"));
+		//String isPreConfigured = validate(info.getAttribute("IsPreConfigured"));
+		//String isPickupable = validate(info.getAttribute("IsPickupAllowed"));
+		//String isShippable = validate(info.getAttribute("IsShippingAllowed"));
+		//String configurationKey = validate(info.getAttribute("ConfigurationKey"));
+		String deschtml = validate(XPEDXMyItemsUtils.formatEscapeCharactersHtml(desc));
+		/*String isConfigurableBundle = "N";
+		if("BUNDLE".equals(kitCode) && "Y".equals(isConfigurable))
+			isConfigurableBundle = "Y";
+		*/
+		String itemCurrency = validate((String) findValue(tag.getCurrency()));
+		boolean isGuestUser = (Boolean)findValue("guestUser");
+		
+		XPEDXUtilBean utilBean = new XPEDXUtilBean();
+		String formattedUnitprice = validate(utilBean.formatPriceWithCurrencySymbol(tag.getCtx(),itemCurrency,myPrice));
+		boolean hasTier = false;
+		
+		sb.append("itemid: \"").append(itemID).append("\",");
+		sb.append("itemkey: \"").append(itemKey).append("\",");
+		sb.append("uom: \"").append(TextUtils.htmlEncode(unitOfMeasure)).append("\",");
+		sb.append("name: \"").append(XPEDXMyItemsUtils.formatEscapeCharactersHtml(shortDesc)).append("\",");
+		sb.append("listprice: \"");
+			
+		String tierPrice = "";
+		List<Element> tierPriceList = tag.getPLLineMap().get(itemID);
+		if(tierPriceList != null) {
+			for (Element tierPrices : tag.getPLLineMap().get(itemID)) {
+				String tierQty = tierPrices.getAttribute("FromQuantity");
+				tierQty = (tierQty == null || "".equals(tierQty)) ? "1" : tierQty;
+				hasTier = true;
+				tierPrice = validate(tierPrices.getAttribute("ListPrice"));
+				Element extnTierPrice = xmlUtils.getChildElement(tierPrices, "Extn");
+				String tierPriceUOM = validate(extnTierPrice.getAttribute("ExtnTierUom"));
+				String PriceUOM = validate(extnTierPrice.getAttribute("ExtnPricingUom"));
+				String formattedTierUnitprice = validate(utilBean.formatPriceWithCurrencySymbol(tag.getCtx(),itemCurrency,tierPrice));
+				if(formattedTierUnitprice == null || "".equals(formattedTierUnitprice))
+					sb.append("<br/>");
+				else {
+					sb.append(TextUtils.htmlEncode(com.sterlingcommerce.xpedx.webchannel.utilities.XPEDXWCUtils.getFormattedQty(tierQty)));
+					sb.append("&nbsp;");
+					try {
+						sb.append(TextUtils.htmlEncode(com.sterlingcommerce.xpedx.webchannel.utilities.XPEDXWCUtils.getFormattedUOMCode(tierPriceUOM))).append("-");
+					} catch (Exception e) {
+						sb.append(TextUtils.htmlEncode(tierPriceUOM)).append("-");
+					} 
+					sb.append(formattedTierUnitprice).append("/");
+					try {
+						sb.append(TextUtils.htmlEncode(com.sterlingcommerce.xpedx.webchannel.utilities.XPEDXWCUtils.getFormattedUOMCode(PriceUOM)));
+					} catch (Exception e) {
+						sb.append(TextUtils.htmlEncode(PriceUOM));
+					} 
+					sb.append("<br/>");
+				}
+			}
+		}
+		
+		if(hasTier == false && !"".equals(formattedUnitprice)) {
+			sb.append(formattedUnitprice).append(" /");
+			try {
+				sb.append(TextUtils.htmlEncode(com.sterlingcommerce.xpedx.webchannel.utilities.XPEDXWCUtils.getFormattedUOMCode(unitOfMeasure)));
+			} catch (Exception e) {
+				sb.append(TextUtils.htmlEncode(unitOfMeasure));
+			} 
+		}
+		sb.append("\",");
+		sb.append("icon: \"").append(pImg).append("\",");
+		//sb.append("icon: \"").append("https://www.xpedx.com/swc/xpedx/images/INF_150x150.jpg").append("\",");
+		sb.append("tabidx: \"").append(tabidx).append("\",");
+		
+		if(isGuestUser == false) {
+			try {
+				if(Integer.parseInt(orderMultiple) > 1) {
+					String uomDesc = TextUtils.htmlEncode(com.sterlingcommerce.xpedx.webchannel.utilities.XPEDXWCUtils.getUOMDescription(unitOfMeasure));
+					sb.append("uomLink: \"")
+						.append("<div class=\\\"notice\\\" style=\\\"margin-right:5px; font-weight: normal;float:right; display:inline;\\\">")
+						.append(tag.getOrderMultipleString()).append(orderMultiple).append(" ")
+						.append(uomDesc).append("</div>\",");
+					sb.append("uomDesc: \"").append(uomDesc).append("\",");
+				}
+				else {
+					sb.append("uomLink: \"\",");
+					sb.append("uomDesc: \"\",");
+				}
+			} catch (Exception e) {
+				sb.append("uomLink: \"\",");
+				sb.append("uomDesc: \"\",");
+			}
+		}
+		
+		sb.append("price: \"").append(TextUtils.htmlEncode(tierPrice)).append("\",");
+		tabidx++;
+		sb.append("buttons: \"").append("<ul id=\\\"prodlist\\\">").append(deschtml).append("</ul>\",");
+		sb.append("material: \"").append(TextUtils.htmlEncode(b2cMaterial)).append("\",");
+		sb.append("form: \"").append(TextUtils.htmlEncode(b2cForm)).append("\",");
+		sb.append("capacity: \"").append(TextUtils.htmlEncode(b2cCapacity)).append("\",");
+		sb.append("model: \"").append(TextUtils.htmlEncode(b2cModel)).append("\",");
+		sb.append("ply: \"").append(TextUtils.htmlEncode(b2cPly)).append("\",");
+		sb.append("size: \"").append(TextUtils.htmlEncode(b2cSize)).append("\",");
+		sb.append("color: \"").append(TextUtils.htmlEncode(b2cColor)).append("\",");
+		sb.append("basis: \"").append(TextUtils.htmlEncode(b2cBasis)).append("\",");
+		sb.append("mwt: \"").append(TextUtils.htmlEncode(b2cMwt)).append("\",");
+		sb.append("vendorNumber: \"").append(TextUtils.htmlEncode(b2cVendorNumber)).append("\",");
+		sb.append("stocked: \"");
+		if(!"W".equals(b2cstockStatus)) {
+			sb.append("M");
+		}
+		sb.append("\",");
+		sb.append("cert: \"");
+		if("Y".equals(b2cCert)) {
+			sb.append("<span><img width=\\\"20\\\" height=\\\"20\\\" src='<s:url value='/xpedx/images/catalog/green-e-logo_small.png'/>'</span>");
+		}
+		sb.append("\",");
+		sb.append("qtyGreaterThanZeroMsg: \"").append(tag.getQtyString()).append("\",");
+		sb.append("partno: \"");
+		
+		if("2".equals(tag.getWcContext().getWCAttribute("customerUseSKU"))) {
+			sb.append(com.sterlingcommerce.xpedx.webchannel.common.XPEDXConstants.MANUFACTURER_ITEM_LABEL)
+				.append(": ").append(TextUtils.htmlEncode(validate(skuMap.get("MPN"))));
+		} else if("3".equals(tag.getWcContext().getWCAttribute("customerUseSKU"))) {
+			sb.append(com.sterlingcommerce.xpedx.webchannel.common.XPEDXConstants.MPC_ITEM_LABEL)
+			.append(": ").append(TextUtils.htmlEncode(validate(skuMap.get("MPC"))));
+		} else if("1".equals(tag.getWcContext().getWCAttribute("customerUseSKU"))) {
+			sb.append(com.sterlingcommerce.xpedx.webchannel.common.XPEDXConstants.CUSTOMER_ITEM_LABEL)
+			.append(": ").append(TextUtils.htmlEncode(validate(skuMap.get("CPN"))));
+		}
+		sb.append("\",");
+		
+		sb.append("uomdisplay: \"<div class=\'uom-select\'><select name='itemUomList' id='itemUomList_")
+			.append(itemID).append("'>");
+		for (Iterator<Map.Entry <String, String>> iterator = itemUOMList.entrySet().iterator(); iterator.hasNext();) {
+			Map.Entry  pair = (Map.Entry ) iterator.next();
+			sb.append("<option value='").append(pair.getKey()).append("'");
+			if(pair.getValue().toString().equals(defaultUOM))
+				sb.append(" selected='selected' ");
+			sb.append(">").append(TextUtils.htmlEncode(pair.getValue().toString())).append("</option>");
+		}
+		sb.append("</select><input type='hidden' id='orderMultiple_");
+		sb.append(itemID).append("' value='");
+		sb.append(orderMultiple).append("'/></div>\",");
+		sb.append("itemtypedesc: \"");
+		if(!"W".equals(b2cstockStatus) && isGuestUser == false) {
+			sb.append("<div class=\'mill-mfg\'>Mill / Mfg. Item<span class=\'addl-chg\'> - Additional charges may apply</span></div>");
+		}
+		sb.append("\",");
+		sb = parseData(sb);
+		
+		try {
+			writer.append((CharSequence) sb);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return super.end(writer, body);
+	}
+	
+	private String validate(String str) {
+		return (str==null ? "" : str);
+	}
+	
+	/**
+	 * The data in SB is used by the javascript. Use this method to test and parse any 
+	 * invalid character in here.
+	 * @param sb
+	 * @return
+	 */
+	private StringBuffer parseData(StringBuffer sb) {
+		int startIndex = -1;
+		while((startIndex = sb.indexOf("//")) >= 0) {
+			sb = sb.replace(startIndex, (startIndex+2), "/");
+		}
+		return sb;
+	}
+	
+	XPEDXItemsDataTemplateTag tag;
+	HttpServletRequest req;
+
+}
---
+++ src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/catalog/XPEDXItemsDataTemplateTag.java
@@ -0,0 +1,166 @@
+/**
+ * 
+ */
+package com.sterlingcommerce.xpedx.webchannel.catalog;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.components.Component;
+import org.apache.struts2.views.jsp.ComponentTagSupport;
+import org.w3c.dom.Element;
+
+import com.opensymphony.xwork2.util.ValueStack;
+import com.sterlingcommerce.ui.web.framework.context.SCUIContext;
+import com.sterlingcommerce.webchannel.core.IWCContext;
+import com.sterlingcommerce.webchannel.core.WCAttributeScope;
+
+/**
+ * @author vgovindan
+ * This class takes care of building JS objects with the items data for the templates in Catalog search results pages.
+ * 
+ */
+public class XPEDXItemsDataTemplateTag extends ComponentTagSupport {
+
+	/* (non-Javadoc)
+	 * @see org.apache.struts2.views.jsp.ComponentTagSupport#getBean(com.opensymphony.xwork2.util.ValueStack, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	public Component getBean(ValueStack vstack, HttpServletRequest request, HttpServletResponse response) {
+		this.wcContext = (IWCContext) request.getAttribute("Tag_WCContext");
+		this.ctx = wcContext.getSCUIContext();
+		orderMultipleString = (String)request.getAttribute("Tag_orderMultipleString");
+		qtyString = (String)request.getAttribute("Tag_qtyString");
+		
+		itemToItemBranchBeanMap = (HashMap<String, XPEDXItemBranchInfoBean>) wcContext.getWCAttribute("itemToItemBranchBeanMap", WCAttributeScope.SESSION);
+		itemToItemBranchBeanMap = itemToItemBranchBeanMap == null ? new HashMap<String, XPEDXItemBranchInfoBean>() : itemToItemBranchBeanMap;
+		itemMap =  (HashMap<String, HashMap<String, String>>) wcContext.getWCAttribute("itemMap", WCAttributeScope.REQUEST);
+		itemMap = itemMap == null ? new HashMap<String, HashMap<String, String>>() : itemMap;		
+		orderMultipleMap =  (HashMap<String, String>) wcContext.getWCAttribute("orderMultipleMap", WCAttributeScope.REQUEST);
+		orderMultipleMap = orderMultipleMap == null ? new HashMap<String, String>() : orderMultipleMap;
+		defaultShowUOMMap =  (HashMap<String, String>) wcContext.getWCAttribute("defaultShowUOMMap", WCAttributeScope.REQUEST);
+		defaultShowUOMMap = defaultShowUOMMap == null ? new HashMap<String, String>() : defaultShowUOMMap;
+		itemUomHashMap = (HashMap<String, HashMap<String, String>>) wcContext.getWCAttribute("itemUomHashMap", WCAttributeScope.REQUEST);
+		itemUomHashMap = itemUomHashMap == null ? new HashMap<String, HashMap<String, String>>() : itemUomHashMap;
+		PLLineMap = (HashMap<String, List<Element>>) wcContext.getWCAttribute("PLLineMap", WCAttributeScope.REQUEST);
+		PLLineMap = PLLineMap == null ? new HashMap<String, List<Element>>() : PLLineMap;
+		this.component = new XPEDXItemsDataTemplateComponent(vstack, request, response, this);
+		return component;
+	}
+	
+	public Map<String, String> getDefaultShowUOMMap() {
+		return defaultShowUOMMap;
+	}
+
+	public void setDefaultShowUOMMap(Map<String, String> defaultShowUOMMap) {
+		this.defaultShowUOMMap = defaultShowUOMMap;
+	}
+
+	public HashMap<String, String> getOrderMultipleMap() {
+		return orderMultipleMap;
+	}
+
+	public void setOrderMultipleMap(HashMap<String, String> orderMultipleMap) {
+		this.orderMultipleMap = orderMultipleMap;
+	}
+
+	public IWCContext getWcContext() {
+		return wcContext;
+	}
+
+	public void setWcContext(IWCContext wcContext) {
+		this.wcContext = wcContext;
+	}
+
+	public String getQtyString() {
+		return qtyString;
+	}
+
+	public void setQtyString(String qtyString) {
+		this.qtyString = qtyString;
+	}
+
+	public String getOrderMultipleString() {
+		return orderMultipleString;
+	}
+
+	public void setOrderMultipleString(String orderMultipleString) {
+		this.orderMultipleString = orderMultipleString;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public HashMap<String, List<Element>> getPLLineMap() {
+		return PLLineMap;
+	}
+
+	public void setPLLineMap(HashMap<String, List<Element>> pLLineMap) {
+		PLLineMap = pLLineMap;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+	public SCUIContext getCtx() {
+		return ctx;
+	}
+
+	public void setCtx(SCUIContext ctx) {
+		this.ctx = ctx;
+	}
+
+	public HashMap<String, XPEDXItemBranchInfoBean> getItemToItemBranchBeanMap() {
+		return itemToItemBranchBeanMap;
+	}
+
+	public void setItemToItemBranchBeanMap(HashMap<String, XPEDXItemBranchInfoBean> itemToItemBranchBeanMap) {
+		this.itemToItemBranchBeanMap = itemToItemBranchBeanMap;
+	}
+
+	public HashMap<String, HashMap<String, String>> getItemMap() {
+		return itemMap;
+	}
+
+	public void setItemMap(HashMap<String, HashMap<String, String>> itemMap) {
+		this.itemMap = itemMap;
+	}
+
+	public HashMap<String, HashMap<String, String>> getItemUomHashMap() {
+		return itemUomHashMap;
+	}
+
+	public void setItemUomHashMap(HashMap<String, HashMap<String, String>> itemUomHashMap) {
+		this.itemUomHashMap = itemUomHashMap;
+	}
+
+	public String getItemElement() {
+		return itemElement;
+	}
+
+	public void setItemElement(String itemElement) {
+		this.itemElement = itemElement;
+	}
+
+
+	private String itemElement;
+	private IWCContext wcContext;
+	private HashMap<String, HashMap<String, String>> itemUomHashMap;
+	private HashMap<String, HashMap<String, String>> itemMap;
+	private HashMap<String, XPEDXItemBranchInfoBean> itemToItemBranchBeanMap;
+	private HashMap<String, String> orderMultipleMap;
+	private Map <String, String>defaultShowUOMMap;
+	private SCUIContext ctx;
+	private String currency;
+	private HashMap <String, List<Element>> PLLineMap;
+	private String orderMultipleString = "Must be ordered in units of ";
+	private String qtyString = "Qty should be greater than 0.";
+
+}
