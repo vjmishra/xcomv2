a7c94ab05cc074e5623aea6658d6b100167acd5f
Ranjeet Singh <singh.ranjeet@hp.com>
2012-02-10 06:08:35 -0500

    JIRA 3343 -- Changes made for loggers as per log4j config
    ----------------------------------- StarTeam -----------------------------------
    M src/smcfs/dev/src/com/xpedx/nextgen/dashboard/XPXUpdateInvoiceDetailsAPI.java:1.4
    --------------------------------------------------------------------------------

--- src/smcfs/dev/src/com/xpedx/nextgen/dashboard/XPXUpdateInvoiceDetailsAPI.java
+++ src/smcfs/dev/src/com/xpedx/nextgen/dashboard/XPXUpdateInvoiceDetailsAPI.java
@@ -28,7 +28,7 @@ public class XPXUpdateInvoiceDetailsAPI {
 	private static YFCLogCategory log;
 	static
 	{
-	log = YFCLogCategory.instance(XPXUpdateSterlingOrderAPI.class);
+	log = (YFCLogCategory) YFCLogCategory.getLogger("com.xpedx.nextgen.log");
 	}
 
 	public void setProperties(Properties arg0) throws Exception {
