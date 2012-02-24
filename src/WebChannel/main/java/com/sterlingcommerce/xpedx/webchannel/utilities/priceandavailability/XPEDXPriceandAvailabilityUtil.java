5fa260bec0afd46425b951aa16e01ff991b4e9e8
Arun Udhayakumar <udhayakumar.arun@hp.com>
2012-02-24 05:46:14 -0500

    Fix for 3098
    ----------------------------------- StarTeam -----------------------------------
    M src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/utilities/priceandavailability/XPEDXPriceandAvailabilityUtil.java:1.15
    --------------------------------------------------------------------------------

--- src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/utilities/priceandavailability/XPEDXPriceandAvailabilityUtil.java
+++ src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/utilities/priceandavailability/XPEDXPriceandAvailabilityUtil.java
@@ -78,7 +78,7 @@ public class XPEDXPriceandAvailabilityUtil {
 	public static int PANDA_WS_MODE = PANDA_WS_PROD ;
 	
 	//Start- Fix for 3105
-	public static final String WS_PRICEANDAVAILABILITY_WITH_SERVICESTATUSDOWN_ERROR = "Price & Availability is not available. Please try again later or contact Customer Service.";
+	public static final String WS_PRICEANDAVAILABILITY_WITH_SERVICESTATUSDOWN_ERROR = "Real time My Price & Availability is not available at this time. Please contact Customer Service.";
 	public static final String WS_PRICEANDAVAILABILITY_TRANSMISSIONSTATUS_ERROR = "Sorry, there was a problem processing your request. Please contact customer service or try again later.";//Sorry, Price & Availability is not available, please try again or contact customer service.";
 	public static final String WS_PRICEANDAVAILABILITY_HEADERSTATUS_ERROR = "Sorry, we could not verify your account information, please contact your customer service representative for price and availability.";//"Sorry, Price & Availability is not available, please contact customer service.";
 	public static final String WS_PRICEANDAVAILABILITY_LINESTATUS_ERROR = "Sorry, the item information for this item is not valid. Please contact customer service for price and availability.";// "Price & Availability is not available, please try again or contact customer service.";
