8bd8ca262b70663775262f98355d7aad20373080
Aamir Ansari <ansari.aamir@hp.com>
2012-03-02 10:14:58 -0500

    jira 2422 - back button httpS issue - just added LOG.debug to these files 1. XPEDXDraftOrderDetailsAction.java 2. XPEDXMyItemsDetailsAction.java 3. XPEDXReturnItemsRequestAction.java 4. XPEDXProductComparisonAction.java 5. XPEDXDraftOrderSummaryAction.java
    ----------------------------------- StarTeam -----------------------------------
    M src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/catalog/XPEDXProductComparisonAction.java:1.5
    --------------------------------------------------------------------------------

--- src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/catalog/XPEDXProductComparisonAction.java
+++ src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/catalog/XPEDXProductComparisonAction.java
@@ -38,6 +38,13 @@ public class XPEDXProductComparisonAction extends ProductComparisonAction {
 		try {			
 			returnVal = super.execute();
 			/* Begin - Changes made by Mitesh Parikh for 2422 JIRA */
+			LOG.debug("-----------------------------------------------------------------------------------------------------------------------");
+			LOG.debug("jira2422 HTTPServletRequest.getScheme() is " + wcContext.getSCUIContext().getRequest().getScheme());
+			LOG.debug("jira2422 HTTPServletRequest.isSecure() is " + wcContext.getSCUIContext().getRequest().isSecure());
+			LOG.debug("jira2422 HTTPServletRequest.header(user-agent) is " + wcContext.getSCUIContext().getRequest().getHeader("user-agent"));
+			LOG.debug("jira2422 HTTPServletRequest.header(CERT_KEYSIZE) is " + wcContext.getSCUIContext().getRequest().getHeader("CERT_KEYSIZE"));
+			LOG.debug("jira2422 HTTPServletRequest.header(HTTPS_KEYSIZE) is " + wcContext.getSCUIContext().getRequest().getHeader("HTTPS_KEYSIZE"));
+			LOG.debug("-----------------------------------------------------------------------------------------------------------------------");
 			setItemDtlBackPageURL((wcContext.getSCUIContext().getRequest().getRequestURL().append("?").append(wcContext.getSCUIContext().getRequest().getQueryString())).toString());
 			/* End - Changes made by Mitesh Parikh for 2422 JIRA */
 			
