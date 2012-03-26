fda1410a7c1536e80913c754385df06454662fdf
Aamir Ansari <ansari.aamir@hp.com>
2012-03-26 11:27:50 -0400

    jira 2422 - back button httpS issue - added System.out to these files to test secure http protocol 1. XPEDXDraftOrderDetailsAction.java 2. XPEDXMyItemsDetailsAction.java 3. XPEDXReturnItemsRequestAction.java 4. XPEDXProductComparisonAction.java 5. XPEDXDraftOrderSummaryAction.java
    ----------------------------------- StarTeam -----------------------------------
    M src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/services/XPEDXReturnItemsRequestAction.java:1.9
    --------------------------------------------------------------------------------

--- src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/services/XPEDXReturnItemsRequestAction.java
+++ src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/services/XPEDXReturnItemsRequestAction.java
@@ -106,13 +106,22 @@ public class XPEDXReturnItemsRequestAction extends OrderDetailAction {
 		setRequestedBy(getUserDetails(eleUserInfo));
 		getCustomerLineDetails();
 		/* Begin - Changes made by Mitesh Parikh for 2422 JIRA */
-		LOG.debug("-----------------------------------------------------------------------------------------------------------------------");
-		LOG.debug("jira2422 HTTPServletRequest.getScheme() is " + wcContext.getSCUIContext().getRequest().getScheme());
-		LOG.debug("jira2422 HTTPServletRequest.isSecure() is " + wcContext.getSCUIContext().getRequest().isSecure());
-		LOG.debug("jira2422 HTTPServletRequest.header(user-agent) is " + wcContext.getSCUIContext().getRequest().getHeader("user-agent"));
-		LOG.debug("jira2422 HTTPServletRequest.header(CERT_KEYSIZE) is " + wcContext.getSCUIContext().getRequest().getHeader("CERT_KEYSIZE"));
-		LOG.debug("jira2422 HTTPServletRequest.header(HTTPS_KEYSIZE) is " + wcContext.getSCUIContext().getRequest().getHeader("HTTPS_KEYSIZE"));
-		LOG.debug("-----------------------------------------------------------------------------------------------------------------------");
+		try
+		{
+			LOG.debug("-----------------------------------------------------------------------------------------------------------------------");
+			LOG.debug("jira2422 HTTPServletRequest.getScheme() is " + wcContext.getSCUIContext().getRequest().getScheme());
+			LOG.debug("jira2422 HTTPServletRequest.isSecure() is " + wcContext.getSCUIContext().getRequest().isSecure());
+			LOG.debug("jira2422 HTTPServletRequest.header(user-agent) is " + wcContext.getSCUIContext().getRequest().getHeader("user-agent"));
+			LOG.debug("jira2422 HTTPServletRequest.header(CERT_KEYSIZE) is " + wcContext.getSCUIContext().getRequest().getHeader("CERT_KEYSIZE"));
+			LOG.debug("jira2422 HTTPServletRequest.header(HTTPS_KEYSIZE) is " + wcContext.getSCUIContext().getRequest().getHeader("HTTPS_KEYSIZE"));
+			System.out.println("jira2422 " + wcContext.getSCUIContext().getRequest().getScheme() + " " + wcContext.getSCUIContext().getRequest().isSecure());
+			System.out.println("jira2422 " + wcContext.getSCUIContext().getRequest().getHeader("CERT_KEYSIZE") + " " + wcContext.getSCUIContext().getRequest().getHeader("HTTPS_KEYSIZE"));
+			LOG.debug("-----------------------------------------------------------------------------------------------------------------------");
+		}
+		catch(Exception ex)
+		{
+			LOG.debug(ex.getMessage());
+		}
 		XPEDXWCUtils.setItemDetailBackPageURLinSession();
 		/* End - Changes made by Mitesh Parikh for 2422 JIRA */
 		return returnString;
