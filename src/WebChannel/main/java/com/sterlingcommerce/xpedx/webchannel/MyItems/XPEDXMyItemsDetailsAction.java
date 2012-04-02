be5d35d0de3832832825f8f812505804f0f69f6b
Aamir Ansari <ansari.aamir@hp.com>
2012-04-02 08:39:15 -0400

    Jira2422 - Back link on item details page after product comparison - Back link is now relative, earlier it was absolute Removed log.debug and sys outs. 1. XPEDXProductComparisonAction.java 2. XPEDXMyItemsDetailsAction.java 3. XPEDXDraftOrderDetailsAction.java 4. XPEDXDraftOrderSummaryAction.java 5. XPEDXReturnItemsRequestAction.java
    ----------------------------------- StarTeam -----------------------------------
    M src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/MyItems/XPEDXMyItemsDetailsAction.java:1.18
    --------------------------------------------------------------------------------

--- src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/MyItems/XPEDXMyItemsDetailsAction.java
+++ src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/MyItems/XPEDXMyItemsDetailsAction.java
@@ -619,22 +619,6 @@ public class XPEDXMyItemsDetailsAction extends WCMashupAction implements
 	public String execute() {
 		try {
 			/* Begin - Changes made by Mitesh Parikh for 2422 JIRA */
-			try
-			{
-				LOG.debug("-----------------------------------------------------------------------------------------------------------------------");
-				LOG.debug("jira2422 HTTPServletRequest.getScheme() is " + wcContext.getSCUIContext().getRequest().getScheme());
-				LOG.debug("jira2422 HTTPServletRequest.isSecure() is " + wcContext.getSCUIContext().getRequest().isSecure());
-				LOG.debug("jira2422 HTTPServletRequest.header(user-agent) is " + wcContext.getSCUIContext().getRequest().getHeader("user-agent"));
-				LOG.debug("jira2422 HTTPServletRequest.header(CERT_KEYSIZE) is " + wcContext.getSCUIContext().getRequest().getHeader("CERT_KEYSIZE"));
-				LOG.debug("jira2422 HTTPServletRequest.header(HTTPS_KEYSIZE) is " + wcContext.getSCUIContext().getRequest().getHeader("HTTPS_KEYSIZE"));
-				System.out.println("jira2422 " + wcContext.getSCUIContext().getRequest().getScheme() + " " + wcContext.getSCUIContext().getRequest().isSecure());
-				System.out.println("jira2422 " + wcContext.getSCUIContext().getRequest().getHeader("CERT_KEYSIZE") + " " + wcContext.getSCUIContext().getRequest().getHeader("HTTPS_KEYSIZE"));
-				LOG.debug("-----------------------------------------------------------------------------------------------------------------------");
-			}
-			catch(Exception ex)
-			{
-				LOG.debug(ex.getMessage());
-			}
 			if(!isEditMode())
 			{
 				setItemDtlBackPageURL((wcContext.getSCUIContext().getRequest().getRequestURL().append("?").append(wcContext.getSCUIContext().getRequest().getQueryString())).toString());
