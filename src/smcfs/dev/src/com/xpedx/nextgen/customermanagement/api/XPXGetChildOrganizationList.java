16e7ea8a8088600b9b339c048ab7578d210536ce
Ragini Ghare <ghare.ragini@hp.com>
2012-04-30 08:44:33 -0400

    Updated for Performance JIRA 3834. Below are the files checked in: 1. XPXGetChildOrganizationList.java 2. xpedx_webchannel_views.sql 3. XPX_Customer_Hierarchy_view.xml
    ----------------------------------- StarTeam -----------------------------------
    M src/smcfs/dev/src/com/xpedx/nextgen/customermanagement/api/XPXGetChildOrganizationList.java:1.4
    --------------------------------------------------------------------------------

--- src/smcfs/dev/src/com/xpedx/nextgen/customermanagement/api/XPXGetChildOrganizationList.java
+++ src/smcfs/dev/src/com/xpedx/nextgen/customermanagement/api/XPXGetChildOrganizationList.java
@@ -1,6 +1,7 @@
 package com.xpedx.nextgen.customermanagement.api;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import org.w3c.dom.Document;
@@ -68,7 +69,7 @@ public class XPXGetChildOrganizationList implements YIFCustomApi{
 
 		if (customerKey != "") {
 			// get the list of child orgs under this customer
-			getChildOrgList(env, orgListDoucument, orgListElement, customerKey);
+			getChildOrgList(env, orgListDoucument, orgListElement, customerKey,inputElement.getAttribute("OrganizationCode"));
 		}
 
 		return orgListDoucument.getDocument();
@@ -83,6 +84,135 @@ public class XPXGetChildOrganizationList implements YIFCustomApi{
 	 *             Method to form the Organization element(this method gets
 	 *             called recursively to fetch all the child organizations)
 	 */
+	private void getChildOrgList(YFSEnvironment env,
+			YFCDocument orgListDoucment, YFCElement orgListElement,
+			String customerKey,String organizationCode) throws RemoteException {
+		// Form the input to fetch the child org list
+		YFCDocument inputCustomerDocument = YFCDocument
+				.createDocument("XPXCustomerHierarchyView");
+		YFCElement inputCustomerElement = inputCustomerDocument
+				.getDocumentElement();
+		YFCElement complexQueryElement = inputCustomerDocument
+		.createElement("ComplexQuery");
+		
+		YFCElement orElement = inputCustomerDocument
+		.createElement("Or");
+		complexQueryElement.appendChild(orElement);
+		YFCElement expElement = inputCustomerDocument
+		.createElement("Exp");
+		expElement.setAttribute("Name", "ShipToCustomerKey");
+		expElement.setAttribute("Value", customerKey);
+		expElement.setAttribute("QryType", "FLIKE");
+		orElement.appendChild(expElement);
+		
+		YFCElement exp1Element = inputCustomerDocument
+		.createElement("Exp");
+		exp1Element.setAttribute("Name", "BillToCustomerKey");
+		exp1Element.setAttribute("Value", customerKey);
+		exp1Element.setAttribute("QryType", "FLIKE");
+		orElement.appendChild(exp1Element);
+		
+		YFCElement exp2Element = inputCustomerDocument
+		.createElement("Exp");
+		exp2Element.setAttribute("Name", "SAPCustomerKey");
+		exp2Element.setAttribute("Value", customerKey);
+		exp2Element.setAttribute("QryType", "FLIKE");
+		orElement.appendChild(exp2Element);
+		
+		YFCElement exp3Element = inputCustomerDocument
+		.createElement("Exp");
+		exp3Element.setAttribute("Name", "MSAPCustomerKey");
+		exp3Element.setAttribute("Value", customerKey);
+		exp3Element.setAttribute("QryType", "FLIKE");
+		orElement.appendChild(exp3Element);
+		inputCustomerElement.appendChild(complexQueryElement);
+		YFCDocument outputCustomerListTemplateDoc = YFCDocument
+				.createDocument("XPXCustHierarchyViewList");
+		YFCElement outputCustomerListTemplateElement = outputCustomerListTemplateDoc
+				.getDocumentElement();
+		outputCustomerListTemplateElement.setAttribute("TotalNumberOfRecords",
+				"");
+		YFCElement outputCustomerTemplateElement = outputCustomerListTemplateDoc
+				.createElement("XPXCustHierarchyView");
+		outputCustomerListTemplateElement
+				.appendChild(outputCustomerTemplateElement);
+		env.setApiTemplate("XPXCustomerHierarchyViewService", outputCustomerListTemplateDoc
+				.getDocument());
+		Document outputCustomerDocument = api.executeFlow(env, "XPXCustomerHierarchyViewService",
+				inputCustomerDocument.getDocument());
+		env.clearApiTemplate("XPXCustomerHierarchyViewService");
+		Element outputCustomerElement = outputCustomerDocument
+				.getDocumentElement();
+		if (!outputCustomerElement.getAttribute("TotalNumberOfRecords").equals(
+				"0")) {
+			createOrganization(outputCustomerElement,customerKey,orgListElement,
+			orgListDoucment,organizationCode);
+			
+		}
+	}
+	
+	private void createOrganization(Element outputCustomerElement,String customerKey,YFCElement orgListElement,
+			YFCDocument orgListDoucment,String organizationCode)
+	{
+		ArrayList<Element> childElementList=SCXmlUtil.getElements(outputCustomerElement, "XPXCustHierarchyView");
+		int root=0;
+		
+		if(childElementList != null && childElementList.size()>0)
+		{
+			Element rootElement =childElementList.get(0);
+			if(customerKey.equals(rootElement.getAttribute("ShipToCustomerKey")))
+				root=0;
+			if(customerKey.equals(rootElement.getAttribute("BillToCustomerKey")))
+				root=1;
+			if(customerKey.equals(rootElement.getAttribute("SAPCustomerKey")))
+				root=2;
+			if(customerKey.equals(rootElement.getAttribute("MSAPCustomerKey")))
+				root=3;
+			getCustomer(childElementList,orgListElement,orgListDoucment,organizationCode,root);
+		}
+		
+	}
+	private void getCustomer(ArrayList<Element> childElementList,YFCElement orgListElement,YFCDocument orgListDoucment,
+			String organizationCode,int root)
+	{
+		String customersArr[]={"ShipTo","BillTo","SAP","MSAP"};	
+		ArrayList<String> addedCustomer=new ArrayList<String>();
+		for(int i=0;i<childElementList.size();i++)
+		{
+			Element customerElement=childElementList.get(i);
+			for(int j=0;j<root;j++)
+			{
+				if(!addedCustomer.contains(customerElement.getAttribute(customersArr[j]+"CustomerID")))
+				{
+						YFCElement orgElement = orgListDoucment.createElement("Organization");
+						orgElement.setAttribute("OrganizationCode", organizationCode);
+						orgElement.setAttribute("CustomerID", customerElement.getAttribute(customersArr[j]+"CustomerID"));
+						addedCustomer.add(customerElement.getAttribute(customersArr[j]+"CustomerID"));
+						orgElement.setAttribute("ParentCustomerID", customerElement.getAttribute(customersArr[j+1]+"CustomerID"));
+						if(j==0)
+							orgElement.setAttribute("CustomerSuffixType", "S");
+						if(j==1)
+							orgElement.setAttribute("CustomerSuffixType", "B");
+						if(j==2)
+							orgElement.setAttribute("CustomerSuffixType", "C");
+						orgElement.setAttribute("CustomerName", customerElement.getAttribute(customersArr[j]+"CustomerName"));
+						orgElement.setAttribute("SAPParentAccNo", customerElement.getAttribute(customersArr[j]+"SAPParentAccNo"));
+						orgElement.setAttribute("SAPNumber", customerElement.getAttribute(customersArr[j]+"SAPNumber"));
+						orgListElement.appendChild(orgElement);
+				}
+			}
+		}
+	}
+		
+	/**
+	 * @param env
+	 * @param orgListDoucment
+	 * @param orgListElement
+	 * @param customerKey
+	 * @throws RemoteException
+	 *             Method to form the Organization element(this method gets
+	 *             called recursively to fetch all the child organizations)
+	 *//*
 	private void getChildOrgList(YFSEnvironment env,
 			YFCDocument orgListDoucment, YFCElement orgListElement,
 			String customerKey) throws RemoteException {
@@ -119,8 +249,10 @@ public class XPXGetChildOrganizationList implements YIFCustomApi{
 		outputCustomerTemplateElement.appendChild(outputParentCustomerTemplateElement);
 		env.setApiTemplate("getCustomerList", outputCustomerListTemplateDoc
 				.getDocument());
+		// System.out.println(YFCDocument.getDocumentFor(inputCustomerDocument.getDocument()));
 		Document outputCustomerDocument = api.invoke(env, "getCustomerList",
 				inputCustomerDocument.getDocument());
+		// System.out.println(YFCDocument.getDocumentFor(outputCustomerDocument));
 		env.clearApiTemplate("getCustomerList");
 		Element outputCustomerElement = outputCustomerDocument
 				.getDocumentElement();
@@ -167,10 +299,7 @@ public class XPXGetChildOrganizationList implements YIFCustomApi{
 						customerKeyForChildOrg);
 
 			}
-		}
-
-	}
-
+		}*/
 	/**
 	 * @param env
 	 * @param inXML
@@ -209,17 +338,20 @@ public class XPXGetChildOrganizationList implements YIFCustomApi{
 				.appendChild(outputCustomerTemplateElement);
 		env.setApiTemplate("getCustomerList", outputCustomerListTemplateDoc
 				.getDocument());
+		// System.out.println(inputCustomerDocument);
+		// System.out.println(YFCDocument.getDocumentFor(inputCustomerDocument.getDocument()));
 		// Document outputCustomerDocument = api.invoke(env, "getCustomerList",
 		// inputCustomerDocument.getDocument());
 		Document outputCustomerDocument = api.invoke(env, "getCustomerList",
 				inputCustomerDocument.getDocument());
+		// System.out.println(YFCDocument.getDocumentFor(outputCustomerDocument));
 		env.clearApiTemplate("getCustomerList");
 		if (!outputCustomerDocument.getDocumentElement().getAttribute(
 				"TotalNumberOfRecords").equals("0")) {
 			Element outputCustomerElement = (Element) outputCustomerDocument
 					.getElementsByTagName("Customer").item(0);
 			customerKey = outputCustomerElement.getAttribute("CustomerKey");
-
+			//System.out.println("CustomerKey" + customerKey);
 		}
 		return customerKey;
 	}
