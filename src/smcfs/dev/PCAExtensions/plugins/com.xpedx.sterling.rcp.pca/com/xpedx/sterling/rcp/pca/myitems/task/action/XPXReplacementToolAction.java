5cf788dcdcaef4308ad4ea736e964f1015d3d4c7
Rakesh Mishra <mishra.rakesh@hp.com>
2012-05-15 07:53:02 -0400

    CR#3000 Unnecessary imports removed.
    ----------------------------------- StarTeam -----------------------------------
    M src/smcfs/dev/PCAExtensions/plugins/com.xpedx.sterling.rcp.pca/com/xpedx/sterling/rcp/pca/myitems/task/action/XPXReplacementToolAction.java:1.1
    --------------------------------------------------------------------------------

--- src/smcfs/dev/PCAExtensions/plugins/com.xpedx.sterling.rcp.pca/com/xpedx/sterling/rcp/pca/myitems/task/action/XPXReplacementToolAction.java
+++ src/smcfs/dev/PCAExtensions/plugins/com.xpedx.sterling.rcp.pca/com/xpedx/sterling/rcp/pca/myitems/task/action/XPXReplacementToolAction.java
@@ -5,18 +5,14 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.part.WorkbenchPart;
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-import com.xpedx.sterling.rcp.pca.myitems.editor.XPXReplacementToolEditor;
 import com.xpedx.sterling.rcp.pca.myitems.screen.XPXMyItemsReplacementToolPanel;
 import com.yantra.yfc.rcp.YRCDesktopUI;
 import com.yantra.yfc.rcp.YRCDialog;
 import com.yantra.yfc.rcp.YRCEditorInput;
-import com.yantra.yfc.rcp.YRCPlatformUI;
 import com.yantra.yfc.rcp.YRCRelatedTask;
 import com.yantra.yfc.rcp.YRCRelatedTaskAction;
-import com.yantra.yfc.rcp.YRCSharedTaskOutput;
 import com.yantra.yfc.rcp.YRCXmlUtils;
 
 public class XPXReplacementToolAction extends YRCRelatedTaskAction {
