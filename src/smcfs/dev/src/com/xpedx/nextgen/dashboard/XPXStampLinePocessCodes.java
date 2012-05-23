448466627c7aab7bb96a21f9600bdd28e8fcf884
Ranjeet Singh <singh.ranjeet@hp.com>
2012-05-23 07:22:13 -0400

    JIRA#3879(Onbehalf Of Staney) - Changes made to improve the order place performance by asynchronising the data picked up to send order confirmation email.
    Files Modified,
    1. XPXEmailHandlerAPI.java
    2. XPXInvokeOrderPlaceActions.java
    3. XPXStampLineProcessCodes.java
    
    ----------------------------------- StarTeam -----------------------------------
    M src/smcfs/dev/src/com/xpedx/nextgen/dashboard/XPXStampLinePocessCodes.java:1.5
    --------------------------------------------------------------------------------

--- src/smcfs/dev/src/com/xpedx/nextgen/dashboard/XPXStampLinePocessCodes.java
+++ src/smcfs/dev/src/com/xpedx/nextgen/dashboard/XPXStampLinePocessCodes.java
@@ -17,65 +17,41 @@ import com.yantra.yfs.japi.YFSEnvironment;
 
 public class XPXStampLinePocessCodes implements YIFCustomApi
 {
-	
 	private static YFCLogCategory log;
 	private static YIFApi api = null;
 
 	static {
 		log = (YFCLogCategory) YFCLogCategory.getLogger("com.xpedx.nextgen.log");
-		try 
-		{
+		try {
 			api = YIFClientFactory.getInstance().getApi();
 		}
 		catch (YIFClientCreationException e1) {
-
 			e1.printStackTrace();
 		}
 	}
 	
-	public Document stampLineProcessCodes(YFSEnvironment env, Document inputXML)
-	{
+	public Document stampLineProcessCodes(YFSEnvironment env, Document inputXML) {
 		
 		if(inputXML != null){
-			log.info("The input to stampLineProcessCodes in XPXStampLinePocessCodes is : " + SCXmlUtil.getString(inputXML));
-		}
-		Element inputDocRoot = inputXML.getDocumentElement();
-		if(log.isDebugEnabled()){
-			log.debug("StampLineProcessCodes_inputDocRoot :" + SCXmlUtil.getString(inputDocRoot));
+			log.info("XPXStampLineProcessCodes_InXML: " + SCXmlUtil.getString(inputXML));
 		}
 		
-		
+		Element inputDocRoot = inputXML.getDocumentElement();	
 		Element orderLinesElement = (Element)inputDocRoot.getElementsByTagName(XPXLiterals.E_ORDER_LINES).item(0);
-		
 		NodeList orderLineList = orderLinesElement.getElementsByTagName(XPXLiterals.E_ORDER_LINE);
 		
-		for(int i=0; i<orderLineList.getLength(); i++)
-		{
+		for(int i=0; i<orderLineList.getLength(); i++) {
 			Element orderLineElement = (Element)orderLineList.item(i);
-			
 			String lineProcessCode = orderLineElement.getAttribute(XPXLiterals.A_LINE_PROCESS_CODE);
-			
-			if(lineProcessCode == null || lineProcessCode.trim().length()== 0)
-			{
+			if(lineProcessCode == null || lineProcessCode.trim().length()== 0) {
 				lineProcessCode = "A"; // Hard coding A for new line additions
-				
 				orderLineElement.setAttribute(XPXLiterals.A_LINE_PROCESS_CODE, lineProcessCode);
 			}
-			
 		}
-		
-		//retrieve ExtnWebConfNum from inputDocRoot and set in txnobject
-		Element extnElem = (Element) inputDocRoot.getElementsByTagName("Extn").item(0);
-		String strExtnWebConfNum = extnElem.getAttribute("ExtnWebConfNum");
-		env.setTxnObject("strExtnWebConfNum", strExtnWebConfNum);
-		
-		
 		return inputXML;
 	}
 
 	public void setProperties(Properties arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
-
 }
