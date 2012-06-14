f041aca39f95359e064e3db3ac9f8725c4256caa
Mohammad Balkhi <balkhi.mohammad@hp.com>
2012-06-14 08:06:53 -0400

    Checked in On Behalf of Anil.(No JIRA# - Checked in as per email received from Ramesh) Changes has been made to add an Exception ID when exception has been logged in CENT.
    ----------------------------------- StarTeam -----------------------------------
    M src/smcfs/dev/src/com/xpedx/nextgen/common/cent/ErrorLogger.java:1.11
    --------------------------------------------------------------------------------

--- src/smcfs/dev/src/com/xpedx/nextgen/common/cent/ErrorLogger.java
+++ src/smcfs/dev/src/com/xpedx/nextgen/common/cent/ErrorLogger.java
@@ -3,9 +3,8 @@ package com.xpedx.nextgen.common.cent;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Random;
 
-import org.apache.log4j.Logger;
-import org.apache.lucene.util.ToStringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -25,39 +24,29 @@ public class ErrorLogger {
 	 */
 	private static YIFApi api = null;
 	private static YFCLogCategory yfcLogCatlog;
-	//Commenting this line as one logger is enough and appenders can be controlled in config file.
-	//private static Logger log4jLogger;
+	
 	static {
-		
-		//Please do not change anything here this is very specific to cent logging  and if the getlogger path going to change this will be conflict with log4jconfig.custom.xml
-		//cent_appender entry for logging
-				yfcLogCatlog = (YFCLogCategory) YFCLogCategory.getLogger("com.xpedx.nextgen.cent");
-		//Commenting this line as one logger is enough and appenders can be controlled in config file.
-		//log4jLogger = YFCLogCategory.getLogger("cent");
+		/* Please do not change anything here this is very specific to cent logging  and if the getlogger path going to change this will be conflict with log4jconfig.custom.xml
+		   cent_appender entry for logging */
+		yfcLogCatlog = (YFCLogCategory) YFCLogCategory.getLogger("com.xpedx.nextgen.cent");
 		try {
 			api = YIFClientFactory.getInstance().getApi();
 		} catch (Exception e) {
 			// TO DO
-			// logger.error("IOM_UTIL_0001", e);
 		}
 	}
 
-	public static void log(Error errorObj, YFSEnvironment yfsEnv)
-			 {
-		// get a pojo with error class,trans type, exception object, input xml
-		// in case, error class is EoF or SoF, log to the log file
-		// else, if error class is Application or UnknownError or
-		// UnexpectedInvalid, log to the file as well as the new table with i/p
-		// xml and exception
+	public static void log(Error errorObj, YFSEnvironment yfsEnv) {
+		/* get a pojo with error class,trans type, exception object, input xml in case, error class is EoF or SoF, log to the log file
+		   else, if error class is Application or UnknownError orUnexpectedInvalid, log to the file as well as the new table with i/p xml and exception
 
-		// i/p XML to XPXGetErrorLookupListService
-
-		// <XPXErrorLookup ErrorClass="Unknown Error" SourceSystem="Sterling"
-		// TransType="App"/>
-		try{
+		   InXML to XPXGetErrorLookupListService :
+		   <XPXErrorLookup ErrorClass="Unknown Error" SourceSystem="Sterling" TransType="App"/>  */
+		try {
 			
-			//SAYAN - START
-			
+			Random random = new Random(); 
+			long fraction = (long)(1 * Math.abs(random.nextLong())); 
+			String randomNumber=""+fraction;
 			String userID = yfsEnv.getUserId();
 			String progID = yfsEnv.getProgId();
 			
@@ -69,176 +58,100 @@ public class ErrorLogger {
 			YIFApi api = YIFClientFactory.getInstance().getApi();
 			YFSEnvironment newEnv = api.createEnvironment(envDoc.getDocument());
 			
-			
-		Document getErrorLookupInDoc = XmlUtils
-				.createDocument("XPXErrorLookup");
-		Element xpxErrorLookupRootElem = getErrorLookupInDoc
-				.getDocumentElement();
-		if (errorObj != null) {
-			xpxErrorLookupRootElem.setAttribute("ErrorClass", errorObj
-					.getErrorClass());
-			xpxErrorLookupRootElem.setAttribute("SourceSystem", "Sterling");
-			xpxErrorLookupRootElem.setAttribute("TransType", errorObj
-					.getTransType());
-
-			/*yfcLogCatlog.debug("XPXGetErrorLookupListService i/p Doc"
-					+ XmlUtils.getString(getErrorLookupInDoc));
-			log4jLogger.debug("XPXGetErrorLookupListService i/p Doc"
-					+ XmlUtils.getString(getErrorLookupInDoc));*/
-			
-			Document getErrorLookupOutputDoc = api.executeFlow(yfsEnv,
-					"XPXGetErrorLookupListService", getErrorLookupInDoc);
-			/*yfcLogCatlog.debug("XPXGetErrorLookupListService o/p Doc"
-					+ XmlUtils.getString(getErrorLookupOutputDoc));
-			log4jLogger.debug("XPXGetErrorLookupListService o/p Doc"
-					+ XmlUtils.getString(getErrorLookupOutputDoc));*/
-			
-			Element outputRootElement = getErrorLookupOutputDoc
-					.getDocumentElement();
-
-			// Field to hold value of the total number of Elements in response
-			String strTotalRecords = null;
-
-			if (outputRootElement != null) {
-				strTotalRecords = outputRootElement
-						.getAttribute("TotalNumberOfRecords");
-				int intTotalRecords = 0;
-				if (strTotalRecords != null
-						&& !strTotalRecords.equalsIgnoreCase("")) {
-					intTotalRecords = Integer.parseInt(strTotalRecords);
-				}
-
-				// Check if record exists
-				if (intTotalRecords > 0) {
-					Element xpxErrorElem = (Element) outputRootElement
-							.getElementsByTagName("XPXErrorLookup").item(0);
-					/*yfcLogCatlog.debug("Total no of records > 0");
-					log4jLogger.debug("Total no of records > 0");*/
-					if (xpxErrorElem != null) {
-						/*yfcLogCatlog.debug(XmlUtils.getString(xpxErrorElem));
-						log4jLogger.debug(XmlUtils.getString(xpxErrorElem));*/
-						/*String logString = xpxErrorElem
-								.getAttribute("TargetSystem")
-								+ "|"
-								+ xpxErrorElem.getAttribute("SourceSystem")
-								+ "|"
-								+ xpxErrorElem.getAttribute("TransType")
-								+ "|"
-								+ xpxErrorElem.getAttribute("CommMethod")
-								+ "|"
-								+ xpxErrorElem.getAttribute("QueueName")
-								+ "|"
-								+ xpxErrorElem.getAttribute("ErrorClass")
-						        + "|"
-						        + errorObj.getErrorDesc()
-						        + "|"
-						        + errorObj.getException(); */
-						        
-						
-						StringBuffer logString = new StringBuffer(xpxErrorElem.getAttribute("TargetSystem"));
-						logString.append("|");
-						logString.append(xpxErrorElem.getAttribute("SourceSystem"));
-						logString.append("|");
-						logString.append(xpxErrorElem.getAttribute("TransType"));
-						logString.append("|");						
-						//logString.append(xpxErrorElem.getAttribute("CommMethod"));
-						logString.append(checkDateTimeCommMethod(xpxErrorElem.getAttribute("CommMethod")));
-						logString.append("|");
-						logString.append(xpxErrorElem.getAttribute("QueueName"));
-						logString.append("|");
-						logString.append(xpxErrorElem.getAttribute("ErrorClass"));
-						logString.append("|");
-						logString.append(errorObj.getException());
-						
-						     
-						/*yfcLogCatlog.debug("yfcLogCatalog.debug");
-						yfcLogCatlog.info("yfcLogCatlog.info");
-						yfcLogCatlog.error("yfcLogCatlog.error");
-						yfcLogCatlog.verbose("yfcLogCatlog.verbose");
-						log4jLogger.info("log4jLogger.info");
-						log4jLogger.debug("log4jLogger.debug");
-						log4jLogger.error("log4jLogger.error");*/
-
-						/*yfcLogCatlog.debug(logString);
-						yfcLogCatlog.info(logString);*/
-						yfcLogCatlog.error(logString.toString());
-						/*yfcLogCatlog.verbose(logString);
-						log4jLogger.info(logString);
-						log4jLogger.debug(logString);
-						log4jLogger.error(logString);*/
-
+			Document getErrorLookupInDoc = XmlUtils.createDocument("XPXErrorLookup");
+			Element xpxErrorLookupRootElem = getErrorLookupInDoc.getDocumentElement();
+			if (errorObj != null) {
+				xpxErrorLookupRootElem.setAttribute("ErrorClass", errorObj.getErrorClass());
+				xpxErrorLookupRootElem.setAttribute("SourceSystem", "Sterling");
+				xpxErrorLookupRootElem.setAttribute("TransType", errorObj.getTransType());
+				
+				Document getErrorLookupOutputDoc = api.executeFlow(yfsEnv,"XPXGetErrorLookupListService", getErrorLookupInDoc);
+				Element outputRootElement = getErrorLookupOutputDoc.getDocumentElement();
+	
+				// Field to hold value of the total number of Elements in response
+				String strTotalRecords = null;
+				if (outputRootElement != null) {
+					strTotalRecords = outputRootElement.getAttribute("TotalNumberOfRecords");
+					int intTotalRecords = 0;
+					if (strTotalRecords != null && !strTotalRecords.equalsIgnoreCase("")) {
+						intTotalRecords = Integer.parseInt(strTotalRecords);
+					}
+	
+					// Check if record exists
+					if (intTotalRecords > 0) {
+						Element xpxErrorElem = (Element) outputRootElement.getElementsByTagName("XPXErrorLookup").item(0);
+						if (xpxErrorElem != null) {
+							
+							StringBuffer logString = new StringBuffer(xpxErrorElem.getAttribute("TargetSystem"));
+							logString.append("|");
+							logString.append("ExceptionID="+randomNumber);						
+							logString.append("|");
+							logString.append(xpxErrorElem.getAttribute("SourceSystem"));
+							logString.append("|");
+							logString.append(xpxErrorElem.getAttribute("TransType"));
+							logString.append("|");						
+							logString.append(checkDateTimeCommMethod(xpxErrorElem.getAttribute("CommMethod")));
+							logString.append("|");
+							logString.append(xpxErrorElem.getAttribute("QueueName"));
+							logString.append("|");
+							logString.append(xpxErrorElem.getAttribute("ErrorClass"));
+							logString.append("|");
+							logString.append(errorObj.getException());											
+							yfcLogCatlog.error(logString.toString());						
+						}
 					}
 				}
-
-			}
-			// populate the Exception table
-			if (errorObj.getErrorClass() != null
-					/** EOF/SOF conditions added by Arun Sekhar on 31-Jan-2011 for CENT tool integration **/
-					&& !errorObj.getErrorClass().equalsIgnoreCase("")) {
-				if (errorObj.getErrorClass().equalsIgnoreCase("Unknown Error")
-						|| errorObj.getErrorClass().equalsIgnoreCase(
-								"Unexpected / Invalid")
-						|| errorObj.getErrorClass().equalsIgnoreCase(
-								"Application") || errorObj.getErrorClass().equalsIgnoreCase(
-								"EOF") || errorObj.getErrorClass().equalsIgnoreCase(
-								"SOF")) {
-					populateNotificationTable(errorObj, newEnv);
+				
+				// To Populate The Exception Table
+				if (errorObj.getErrorClass() != null && !errorObj.getErrorClass().equalsIgnoreCase("")) {
+					if (errorObj.getErrorClass().equalsIgnoreCase("Unknown Error")
+							|| errorObj.getErrorClass().equalsIgnoreCase("Unexpected / Invalid")
+							|| errorObj.getErrorClass().equalsIgnoreCase("Application") 
+							|| errorObj.getErrorClass().equalsIgnoreCase("EOF") 
+							|| errorObj.getErrorClass().equalsIgnoreCase("SOF")) {
+						populateNotificationTable(errorObj, newEnv,randomNumber);
+					}
 				}
 			}
-
-		}
-		}catch(Exception e){
-			//Commenting this line as one logger is enough and appenders can be controlled in config file.
-			//log4jLogger.error(e.getMessage());
+		} catch (Exception e) {
 			yfcLogCatlog.error(e.getMessage());
 		}
 	}
     
-	//Product Support Readiness, Fix for Jira 2691.
-	private static String checkDateTimeCommMethod(String theCommMethodVal)
-	{
+	private static String checkDateTimeCommMethod(String theCommMethodVal) {
+		
 		String commMethod = theCommMethodVal;
-		try
-		{
+		try {
 			Calendar calendar = Calendar.getInstance();
 			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 			String dateNow = dateFormat.format(calendar.getTime());
-			
 			int theHour = calendar.get(calendar.HOUR_OF_DAY) ;
 			
-			if(commMethod != null && commMethod.equalsIgnoreCase("P1"))
-			{
-				if(calendar.get(calendar.DAY_OF_WEEK) == 7)
-				{
-					if(theHour >= 18 && theHour <= 23)
-					{
+			if(commMethod != null && commMethod.equalsIgnoreCase("P1")) {
+				if(calendar.get(calendar.DAY_OF_WEEK) == 7) {
+					if(theHour >= 18 && theHour <= 23) {
 						commMethod = "P3";
 					}
 				}				
 			}
-		}
-		catch (Exception e){
-			//Commenting this line as one logger is enough and appenders can be controlled in config file.
-			//log4jLogger.error(e.getMessage());
+		} catch (Exception e){
 			yfcLogCatlog.error(e.getMessage());			
 		}
 		return commMethod;		
 	}
 	
-	public static void populateNotificationTable(Error errorObj,
-			YFSEnvironment yfsEnv) {
+	public static void populateNotificationTable(Error errorObj,YFSEnvironment yfsEnv,String randomNumber) {
+		
 		try {
 			Document createErrorNotificatnInDoc = XmlUtils.createDocument("XPXErrorNotification");
-			Element xpxErrorNotifyRootElem = createErrorNotificatnInDoc
-					.getDocumentElement();
+			Element xpxErrorNotifyRootElem = createErrorNotificatnInDoc.getDocumentElement();
 			String exceptionMessage = null;
 			if (errorObj != null) {
-				if (errorObj.getException() != null
-						&& errorObj.getException().getMessage() != null) {
+				if (errorObj.getException() != null && errorObj.getException().getMessage() != null) {
 					exceptionMessage = errorObj.getExceptionMessage();
 					if (YFCObject.isNull(exceptionMessage) || YFCObject.isVoid(exceptionMessage)) {
-						//handle the case where stack reaches more than 3800 char
-						if(errorObj.getException().getMessage().length()>3800) {
+						// Handle the case where stack reaches more than 3800 char
+						if(errorObj.getException().getMessage().length() > 3800) {
 							String StackTrace=((String)errorObj.getException().getMessage());
 							StackTrace=StackTrace.substring(0, 3800);					
 							xpxErrorNotifyRootElem.setAttribute("StackTrace",StackTrace);
@@ -249,53 +162,28 @@ public class ErrorLogger {
 						// To Set Only Exception Message For Order Update/Order Edit Interface.
 						xpxErrorNotifyRootElem.setAttribute("StackTrace",exceptionMessage);
 					}
-					 
-					 /** Added by Arun Sekhar on 31-Jan-20100 for CENT tool logging of EOF/SOF messages **/
-				}else if(null != errorObj.getErrorDesc()){
+				} else if(null != errorObj.getErrorDesc()) {
 					xpxErrorNotifyRootElem.setAttribute("StackTrace",errorObj.getErrorDesc());
-				}
-				else{
+				} else {
 					xpxErrorNotifyRootElem.setAttribute("StackTrace","");
 				}
 				
 				if(errorObj.getInputDoc() != null){	
-					if(XmlUtils.getString(errorObj.getInputDoc()).length()>3800)
-					{
+					if(XmlUtils.getString(errorObj.getInputDoc()).length()>3800) {
 						String XmlFile=XmlUtils.getString(errorObj.getInputDoc()).substring(0, 3800);
 						xpxErrorNotifyRootElem.setAttribute("XmlFile",XmlFile);
+					} else {
+						xpxErrorNotifyRootElem.setAttribute("XmlFile",XmlUtils.getString(errorObj.getInputDoc()));
 					}
-					else
-					{
-					xpxErrorNotifyRootElem.setAttribute("XmlFile",XmlUtils.getString(errorObj.getInputDoc()));
-					}
-				}
-				else{
+				} else {
 					xpxErrorNotifyRootElem.setAttribute("XmlFile","");
 				}
-				Document createErrorNotificatnOutputDoc = api.executeFlow(
-						yfsEnv, "XPXCreateErrorNotificatnService",
-						createErrorNotificatnInDoc);
 				
+				xpxErrorNotifyRootElem.setAttribute("ExceptionID",randomNumber);
+				Document createErrorNotificatnOutputDoc = api.executeFlow(yfsEnv, "XPXCreateErrorNotificatnService",createErrorNotificatnInDoc);
 			}
 		} catch (Exception e) {
 			yfcLogCatlog.error(e.getMessage());
-			//Commenting this line as one logger is enough and appenders can be controlled in config file.
-			//log4jLogger.error(e.getMessage());
 		}
 	}
-
-	// public static void main (String a[]){
-	// ErrorLogger errorLogger = new ErrorLogger();
-	// errorLogger.loger();
-	// }
-	//	
-	// public void loger(){
-	// yfcLogCatlog.debug("yfcLogCatalog.debug");
-	// yfcLogCatlog.info("yfcLogCatlog.info");
-	// yfcLogCatlog.error("yfcLogCatlog.error");
-	// yfcLogCatlog.verbose("yfcLogCatlog.verbose");
-	// log4jLogger.info("log4jLogger.info");
-	// log4jLogger.debug("log4jLogger.debug");
-	// log4jLogger.error("log4jLogger.error");
-	// }
 }
