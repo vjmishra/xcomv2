7f29249b0bb774ad2229692012bef0c60e22dcd4
Manasa Kumar Mohapatro <kumar.manasa@hp.com>
2012-06-21 07:15:41 -0400

    Added For CR#3000
    ----------------------------------- StarTeam -----------------------------------
    M src/smcfs/dev/Foundation/database/oracle/scripts/XPEDX_MIL_BOTHLIST.sql:1.5
    --------------------------------------------------------------------------------

--- src/smcfs/dev/Foundation/database/oracle/scripts/XPEDX_MIL_BOTHLIST.sql
+++ src/smcfs/dev/Foundation/database/oracle/scripts/XPEDX_MIL_BOTHLIST.sql
@@ -1,57 +1,40 @@
-CREATE OR REPLACE VIEW XPEDX_MIL_BOTHLIST 
+CREATE OR REPLACE FORCE VIEW "NG"."XPEDX_MIL_BOTHLIST" ("MY_ITEMS_LIST_KEY", "CUSTOMER_ID", "LIST_NAME", "LIST_DESC", "CREATEUSERNAME", "CREATEUSERID", "MODIFYUSERID", "SHARE_ADMIN_ONLY", "SHARE_PRIVATE", "MODIFYTS", "LIST_ORDER", "CUSTOMER_KEY", "PARENT_CUSTOMER_KEY", "ROOT_CUSTOMER_KEY", "EXTN_SUFFIX_TYPE", "EXTN_CUSTOMER_DIVISION", "ORGANIZATION_NAME", "ORGANIZATION_CODE", "SHARE_CUSTOMER_ID", "MSAPNAME", "TOTALITEMS")
 AS
-SELECT 
-    MIL.MY_ITEMS_LIST_KEY,
-    MIL.CUSTOMER_ID,
-    MIL.LIST_NAME,
-    MIL.LIST_DESC,
-    MIL.CREATEUSERNAME,
-    MIL.CREATEUSERID,
-    MIL.MODIFYUSERID,
-    MIL.SHARE_ADMIN_ONLY,
-    MIL.SHARE_PRIVATE,
-    MIL.MODIFYTS,
-	MIL.LIST_ORDER,
-    CUST.CUSTOMER_KEY,
-    CUST.PARENT_CUSTOMER_KEY,
-    CUST.ROOT_CUSTOMER_KEY,
-    CUST.EXTN_SUFFIX_TYPE,
-    CUST.EXTN_CUSTOMER_DIVISION,
-    NVL(MILS.MY_ITEMS_LIST_SHARE_KEY,'99999') AS MY_ITEMS_LIST_SHARE_KEY,
-    MILS.CUSTOMER_ID AS SHARE_CUSTOMER_ID,
-    MILS.CUSTOMER_PATH,
-    ORG.ORGANIZATION_NAME,
-    ORG.ORGANIZATION_CODE,
-	MSAPORG.ORGANIZATION_NAME || ' (' || substr(MSAPCUST.CUSTOMER_ID,4,10) || ')' AS MSAPNAME,
-    COUNT(MY_ITEMS_KEY) AS TOTALITEMS
-FROM xpedx_my_items_list MIL
-inner join yfs_customer CUST on (MIL.Customer_id) = trim(CUST.Customer_id)
-inner join yfs_customer MSAPCUST on trim(CUST.ROOT_CUSTOMER_KEY) = trim(MSAPCUST.CUSTOMER_KEY) 
-inner join yfs_organization MSAPORG on trim(MSAPCUST.CUSTOMER_ID) = trim(MSAPORG.ORGANIZATION_KEY) 
-LEFT OUTER JOIN xpedx_my_items_items ITEM on trim(MIL.MY_ITEMS_LIST_KEY) = trim(ITEm.MY_ITEMS_LIST_KEY)
-LEFT OUTER JOIN xpedx_my_items_list_share MILS ON trim(MIL.MY_ITEMS_LIST_KEY) = trim(mils.MY_ITEMS_LIST_KEY)
-LEFT OUTER JOIN yfs_organization ORG ON trim(cust.EXTN_CUSTOMER_DIVISION) = replace(trim(ORG.organization_code),'_M','')
-GROUP BY
-    MIL.MY_ITEMS_LIST_KEY,
-    MIL.CUSTOMER_ID,
-    MIL.LIST_NAME,
-    MIL.LIST_DESC,
-    MIL.CREATEUSERNAME,
-    MIL.CREATEUSERID,
-    MIL.MODIFYUSERID,
-    MIL.SHARE_ADMIN_ONLY,
-    MIL.SHARE_PRIVATE,
-    MIL.MODIFYTS,
-	MIL.LIST_ORDER,
-    CUST.CUSTOMER_KEY,
-    CUST.PARENT_CUSTOMER_KEY,
-    CUST.ROOT_CUSTOMER_KEY,
-    CUST.EXTN_SUFFIX_TYPE,
-    CUST.EXTN_CUSTOMER_DIVISION,
-    MILS.MY_ITEMS_LIST_SHARE_KEY,
-    MILS.CUSTOMER_ID,
-    MILS.CUSTOMER_PATH,
-    ORG.ORGANIZATION_NAME,
-    ORG.ORGANIZATION_CODE,
-	MSAPORG.ORGANIZATION_NAME || ' (' || substr(MSAPCUST.CUSTOMER_ID,4,10) || ')'
-
+  SELECT DISTINCT mil.my_items_list_key,
+    mil.customer_id,
+    mil.list_name,
+    mil.list_desc,
+    mil.createusername,
+    mil.createuserid,
+    mil.modifyuserid,
+    mil.share_admin_only,
+    mil.share_private,
+    mil.modifyts,
+    mil.list_order,
+    cust.customer_key,
+    cust.parent_customer_key,
+    cust.root_customer_key,
+    cust.extn_suffix_type,
+    cust.extn_customer_division,
+    org.organization_name,
+    org.organization_code,
+    mils.customer_id AS share_customer_id,
+    msaporg.organization_name
+    || ' ('
+    || SUBSTR (msapcust.customer_id, 4, 10)
+    || ')' AS msapname,
+    (SELECT COUNT (*)
+    FROM xpedx_my_items_items item
+    WHERE item.my_items_list_key = mil.my_items_list_key
+    ) AS totalitems
+  FROM xpedx_my_items_list mil
+  INNER JOIN yfs_customer cust
+  ON (mil.customer_id) = trim(cust.customer_id)
+  INNER JOIN yfs_customer msapcust
+  ON TRIM (cust.root_customer_key) = TRIM (msapcust.customer_key)
+  INNER JOIN yfs_organization msaporg
+  ON TRIM (msapcust.customer_id) = TRIM (msaporg.organization_key)
+  LEFT OUTER JOIN xpedx_my_items_list_share mils
+  ON mil.my_items_list_key = mils.my_items_list_key
+  LEFT OUTER JOIN yfs_organization org
+  ON TRIM (mils.division_id) =trim(org.organization_code);
