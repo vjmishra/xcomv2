d1a8518850b5e87f049bd6a008062d740716de0e
Ramya Ur <ur.ramya@hp.com>
2012-07-18 06:54:30 -0400

    Added by Ragini for Jira 4102
    ----------------------------------- StarTeam -----------------------------------
    A src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/home/XPEDXSalesRepLogoutAction.java:1.0
    --------------------------------------------------------------------------------

---
+++ src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/home/XPEDXSalesRepLogoutAction.java
@@ -0,0 +1,59 @@
+package com.sterlingcommerce.xpedx.webchannel.home;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.log4j.Logger;
+import org.w3c.dom.Element;
+import com.yantra.yfc.util.YFCDate;
+import com.sterlingcommerce.webchannel.core.wcaas.Logout;
+import com.sterlingcommerce.xpedx.webchannel.common.XPEDXConstants;
+import com.sterlingcommerce.xpedx.webchannel.utilities.XPEDXWCUtils;
+
+@SuppressWarnings("serial")
+public class XPEDXSalesRepLogoutAction extends Logout {
+	
+	private static final Logger LOG = Logger
+	.getLogger(XPEDXSalesRepLogoutAction.class);
+	
+	public String execute() {
+		String lastLoginDate="";
+		String returnType="WebUser";
+		YFCDate loginDate = new YFCDate();
+    	lastLoginDate = loginDate.getString();
+    	
+    	Map<String, String> attributeMap = new HashMap<String, String>();
+		String custContRefKey = null;
+		boolean createCCExtn = false;
+		String customerContactId=  wcContext.getCustomerContactId();
+		Element xpxCustContExtnEle= XPEDXWCUtils.getXPXCustomerContactExtn(wcContext, customerContactId);
+		if(xpxCustContExtnEle == null)
+			createCCExtn = true;
+		else {
+			custContRefKey = xpxCustContExtnEle.getAttribute("CustContRefKey");
+		}
+		Boolean isSalesRep = null;
+		if(getWCContext().getSCUIContext().getSession().getAttribute("IS_SALES_REP")!=null){
+			isSalesRep=Boolean.valueOf((Boolean) getWCContext().getSCUIContext().getSession().getAttribute("IS_SALES_REP"));
+		}
+		if( isSalesRep!= null && isSalesRep==true){
+			returnType="SalesRepUser";
+		}
+
+		System.out.println(":: *******************XPEDXLogoutAction:returnType= "+returnType);
+		if(custContRefKey!=null && custContRefKey.length()>0)
+			attributeMap.put(XPEDXConstants.XPX_CUSTCONTACT_EXTN_REF_ATTR, custContRefKey);
+		
+		attributeMap.put(XPEDXConstants.XPX_CUSTCONTACT_EXTN_LAST_LOGIN_DATE, lastLoginDate);		
+		
+		Element outDoc = (Element)XPEDXWCUtils.updateXPXCustomerContactExtn(wcContext, customerContactId,createCCExtn, attributeMap);    	
+    	
+		try {
+			super.execute();
+		} catch (Exception e) {
+			LOG.error("Error during logout " + e.getMessage());
+		}
+		
+		return returnType;
+	}
+
+}
