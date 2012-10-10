d6c992bd639df44acb5d526664c478282165fd35
Mitesh Parikh <parikh.mitesh@hp.com>
2012-10-10 02:37:44 -0400

    JIRA# 4321 - Stamping the value of two new attributes : 'ExtnOrdHdrLevelFailedRuleID' added  at order header level]  and 'ExtnOrdLineLevelFailedRuleID' added at order line level. List of files modified for this JIRA : XPXCustomerProfileRuleHold.java XPXCustomerProfileRuleResolve.java XPXCustomerProfileRuleValidation.java XPXCustomerProfileRuleUtil.java
    ----------------------------------- StarTeam -----------------------------------
    M src/smcfs/dev/src/com/xpedx/nextgen/customerprofilerulevalidation/api/XPXCustomerProfileRuleHold.java:1.4
    --------------------------------------------------------------------------------

--- src/smcfs/dev/src/com/xpedx/nextgen/customerprofilerulevalidation/api/XPXCustomerProfileRuleHold.java
+++ src/smcfs/dev/src/com/xpedx/nextgen/customerprofilerulevalidation/api/XPXCustomerProfileRuleHold.java
@@ -9,7 +9,9 @@ import com.sterlingcommerce.baseutil.SCXmlUtil;
 import com.yantra.interop.japi.YIFApi;
 import com.yantra.interop.japi.YIFClientCreationException;
 import com.yantra.interop.japi.YIFClientFactory;
+import com.yantra.yfc.core.YFCIterable;
 import com.yantra.yfc.dom.YFCDocument;
+import com.yantra.yfc.dom.YFCElement;
 import com.yantra.yfc.log.YFCLogCategory;
 import com.yantra.yfs.japi.YFSEnvironment;
 
@@ -59,14 +61,16 @@ public class XPXCustomerProfileRuleHold {
 					"OrderHeaderKey");
 			changeOrderElement.setAttribute("OrderHeaderKey", orderHeaderKey);
 			
-			// To set the override flag to 'Y' for a fulfillment order. 
-			orderType = inXML.getDocumentElement().getAttribute("OrderType");
+			Element extnElement = SCXmlUtil.createChild(changeOrderElement, "Extn");			
+			orderType = inXML.getDocumentElement().getAttribute("OrderType");			
+			// To set the override flag to 'Y' for a fulfillment order.
 			if(orderType != null && !orderType.equalsIgnoreCase("Customer")){
 				changeOrderElement.setAttribute("Override", "Y");
-				Element extnElement = SCXmlUtil.createChild(changeOrderElement, "Extn");
 				extnElement.setAttribute("ExtnWebHoldFlag", "Y");
 				extnElement.setAttribute("ExtnWebHoldReason", XPXCustomerProfileRuleConstant.NEEDS_ATTENTION_HOLD_DESC);
 			}
+			
+			applyOrderRuleIDsToChangeOrder(changeOrderElement, inXML);
 			
 			Element orderHoldTypesElement = SCXmlUtil.createChild(
 					changeOrderElement, "OrderHoldTypes");
@@ -107,5 +111,51 @@ public class XPXCustomerProfileRuleHold {
 				.setAttribute(
 						"Status",
 						XPXCustomerProfileRuleConstant.NEEDS_ATTENTION_ACTIVE_STATUS);
+	}
+	//Added for JIRA 4321
+	private void applyOrderRuleIDsToChangeOrder(Element changeOrderElement, Document inXML){
+		YFCDocument inXMLDoc= YFCDocument.getDocumentFor(inXML);
+		YFCElement inXMLOrderExtn= inXMLDoc.getDocumentElement().getChildElement("Extn");
+		if(inXMLOrderExtn!=null)
+		{
+			Element extnElement = (Element)changeOrderElement.getElementsByTagName("Extn").item(0);			
+			String ordHdrLevelFailedRuleID = inXMLOrderExtn.getAttribute("ExtnOrdHdrLevelFailedRuleID");	
+			extnElement.setAttribute("ExtnOrdHdrLevelFailedRuleID", ordHdrLevelFailedRuleID);
+		}
+		
+		YFCElement inXMLOrderLines = inXMLDoc.getDocumentElement().getChildElement("OrderLines");
+		if(inXMLOrderLines!=null)
+		{
+			Element changeOrderLinesElement = SCXmlUtil.createChild(changeOrderElement, "OrderLines");
+			
+			YFCIterable<YFCElement> inXMLOrderLineItr = inXMLOrderLines.getChildren("OrderLine");
+			while (inXMLOrderLineItr.hasNext()) {
+				YFCElement inXMLOrderLine = inXMLOrderLineItr.next();
+				if (inXMLOrderLine != null) {
+					String lineType=inXMLOrderLine.getAttribute("LineType");
+					if(lineType==null || "".equals(lineType) || "M".equals(lineType) || "C".equals(lineType))
+					{
+						continue;
+					}
+					
+					Element changeOrderLineElement = SCXmlUtil.createChild(changeOrderLinesElement, "OrderLine");					
+					changeOrderLineElement.setAttribute("OrderLineKey", inXMLOrderLine.getAttribute("OrderLineKey"));
+					
+					YFCElement	extnInXMLOrdLine = inXMLOrderLine.getChildElement("Extn");
+					if(extnInXMLOrdLine!=null)
+					{
+						Element extnChangeOrderLineElement = SCXmlUtil.createChild(changeOrderLineElement, "Extn");
+						extnChangeOrderLineElement.setAttribute("ExtnOrdLineLevelFailedRuleID", extnInXMLOrdLine.getAttribute("ExtnOrdLineLevelFailedRuleID"));
+						changeOrderLineElement.appendChild(extnChangeOrderLineElement);
+						
+					}
+					changeOrderLinesElement.appendChild(changeOrderLineElement);
+			
+				}
+			}
+			changeOrderElement.appendChild(changeOrderLinesElement);
+			
+		}
+		
 	}
 }
