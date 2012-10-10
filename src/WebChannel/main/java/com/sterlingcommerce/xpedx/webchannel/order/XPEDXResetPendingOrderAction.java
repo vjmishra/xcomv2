5295f1468a4d9e3d2b0b9f512da1b9208e0390ad
Ramya Ur <ur.ramya@hp.com>
2012-10-10 13:22:37 -0400

    Updated for jira 4306 - XPEDXWCUtils.java XPEDXOrderPlaceAction.java XPEDXResetPendingOrderAction.java XPEDXResetPendingOrderAction.java
    ----------------------------------- StarTeam -----------------------------------
    M src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/order/XPEDXDraftOrderDetailsAction.java:1.42
    M src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/order/XPEDXOrderPlaceAction.java:1.24
    M src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/order/XPEDXResetPendingOrderAction.java:1.4
    --------------------------------------------------------------------------------

--- src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/order/XPEDXResetPendingOrderAction.java
+++ src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/order/XPEDXResetPendingOrderAction.java
@@ -2,12 +2,15 @@ package com.sterlingcommerce.xpedx.webchannel.order;
 
 import com.sterlingcommerce.webchannel.order.ResetPendingOrderAction;
 import com.sterlingcommerce.xpedx.webchannel.common.XPEDXConstants;
+import com.sterlingcommerce.xpedx.webchannel.utilities.XPEDXWCUtils;
 
 public class XPEDXResetPendingOrderAction extends ResetPendingOrderAction {	
 	
 	public String execute()
 	{
-		wcContext.getSCUIContext().getSession().removeAttribute(XPEDXConstants.EDITED_ORDER_HEADER_KEY);
+		//added for jira 4306
+		XPEDXWCUtils.resetEditedOrderShipTo(wcContext);
+		wcContext.getSCUIContext().getSession().removeAttribute(XPEDXConstants.EDITED_ORDER_HEADER_KEY);		
 		return super.execute();
 	}
 }
