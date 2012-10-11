f75a5ed8539f2aa3132d4ff862c5c5bf06336f32
Mitesh Parikh <parikh.mitesh@hp.com>
2012-10-11 06:03:55 -0400

    Changes made to get child [<OrderHoldTypes/>] element of <Order> element  [and not <OrderLine> element
    ----------------------------------- StarTeam -----------------------------------
    M src/smcfs/dev/src/com/xpedx/nextgen/dashboard/XPXISHoldTypeNeedsAttentionCondition.java:1.4
    --------------------------------------------------------------------------------

--- src/smcfs/dev/src/com/xpedx/nextgen/dashboard/XPXISHoldTypeNeedsAttentionCondition.java
+++ src/smcfs/dev/src/com/xpedx/nextgen/dashboard/XPXISHoldTypeNeedsAttentionCondition.java
@@ -3,26 +3,28 @@ package com.xpedx.nextgen.dashboard;
 import java.util.Map;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 
 import com.xpedx.nextgen.common.util.XPXLiterals;
 import com.yantra.interop.japi.YIFApi;
 import com.yantra.interop.japi.YIFClientCreationException;
 import com.yantra.interop.japi.YIFClientFactory;
 import com.yantra.ycp.japi.YCPDynamicConditionEx;
+import com.yantra.yfc.core.YFCIterable;
+import com.yantra.yfc.dom.YFCDocument;
+import com.yantra.yfc.dom.YFCElement;
 import com.yantra.yfc.log.YFCLogCategory;
 import com.yantra.yfs.japi.YFSEnvironment;
 
-public class XPXISHoldTypeNeedsAttentionCondition implements YCPDynamicConditionEx{
+public class XPXISHoldTypeNeedsAttentionCondition implements
+		YCPDynamicConditionEx {
 
 	private static YFCLogCategory log;
 	private static YIFApi api = null;
 	
-	
 	static {
-		log = (YFCLogCategory) YFCLogCategory.getLogger("com.xpedx.nextgen.log");
-		
+		log = (YFCLogCategory) YFCLogCategory
+				.getLogger("com.xpedx.nextgen.log");
+
 		try {
 			api = YIFClientFactory.getInstance().getApi();
 		} catch (YIFClientCreationException e) {
@@ -30,53 +32,45 @@ public class XPXISHoldTypeNeedsAttentionCondition implements YCPDynamicCondition
 			e.printStackTrace();
 		}
 	}
-	
-	
-	public boolean evaluateCondition(YFSEnvironment env, String arg1, Map arg2, Document getOrderDetailsOutput)
-	{
-		
+
+	public boolean evaluateCondition(YFSEnvironment env, String arg1, Map arg2,
+			Document getOrderDetailsOutput) {
+
 		boolean isOrderOnNeedsAttentionHold = false;
+		YFCDocument yfcOrderDetailDoc = YFCDocument.getDocumentFor(getOrderDetailsOutput);
+		YFCElement rootElem = yfcOrderDetailDoc.getDocumentElement();
+		YFCElement orderHoldTypesElem = rootElem
+				.getChildElement(XPXLiterals.E_ORDER_HOLD_TYPES);
+		if (orderHoldTypesElem != null) {
+			YFCIterable<YFCElement> holdTypeItr = orderHoldTypesElem.getChildren(XPXLiterals.E_ORDER_HOLD_TYPE);
+			while (holdTypeItr.hasNext()) {
+				YFCElement orderHoldTypeElem = holdTypeItr.next();
+				if (orderHoldTypeElem != null) {
+					String holdType = orderHoldTypeElem
+							.getAttribute(XPXLiterals.A_HOLD_TYPE);
+					String status = orderHoldTypeElem
+							.getAttribute(XPXLiterals.A_STATUS);
 
-        Element getOrderDetailsOutputRoot = getOrderDetailsOutput.getDocumentElement();
-        
-        Element getOrderDetailsOutputOrderHoldTypes = (Element)getOrderDetailsOutputRoot.getElementsByTagName(XPXLiterals.E_ORDER_HOLD_TYPES).item(0);
-        
-        if(getOrderDetailsOutputOrderHoldTypes != null)
-        {	
-        
-        NodeList getOrderDetailsOutputOrderHoldTypeList = getOrderDetailsOutputOrderHoldTypes.getElementsByTagName(XPXLiterals.E_ORDER_HOLD_TYPE);
-        
-        if(getOrderDetailsOutputOrderHoldTypeList.getLength() > 0)
-        {
-        
-        for(int i=0; i < getOrderDetailsOutputOrderHoldTypeList.getLength(); i++)
-        {
-        	Element orderHoldType = (Element)getOrderDetailsOutputOrderHoldTypeList.item(i);
-        	
-        	String holdType = orderHoldType.getAttribute(XPXLiterals.A_HOLD_TYPE);
-        	
-        	String status = orderHoldType.getAttribute(XPXLiterals.A_STATUS);
-        	
-        	if((holdType.equalsIgnoreCase(XPXLiterals.NEEDS_ATTENTION_HOLD) && status.equalsIgnoreCase(XPXLiterals.HOLD_CREATED_STATUS_ID))
-        			|| (getOrderDetailsOutputRoot.getAttribute(XPXLiterals.A_HAS_ERROR).equalsIgnoreCase(XPXLiterals.BOOLEAN_FLAG_Y)))
-        			{
-        		              log.debug("The order is on hold or has an error---No chained order will be created");
-        		              isOrderOnNeedsAttentionHold = true;  		              
-        		              
-        		              break;
-        			}
-        	
+					if ((holdType.equalsIgnoreCase(XPXLiterals.NEEDS_ATTENTION_HOLD)  && 
+						 status.equalsIgnoreCase(XPXLiterals.HOLD_CREATED_STATUS_ID)) || 
+						(rootElem.getAttribute(XPXLiterals.A_HAS_ERROR).equalsIgnoreCase(XPXLiterals.BOOLEAN_FLAG_Y))) 
+					{
+						log.debug("The order is on hold or has an error---No chained order will be created");
+						isOrderOnNeedsAttentionHold = true;
+						break;
+					}
 
-         }
-        }
+				}
+			}
 
-        }
-        
-        return isOrderOnNeedsAttentionHold;
+		}
+
+		return isOrderOnNeedsAttentionHold;
 	}
+
 	public void setProperties(Map arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
