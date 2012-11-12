38a47b9fc8a57946109d8d4105c243f3545a5a42
Ranjeet Singh <singh.ranjeet@hp.com>
2012-11-12 11:14:35 -0500

    Checkin as part of XBT-217
    ----------------------------------- StarTeam -----------------------------------
    M src/WebChannel/main/webapp/xpedx/js/sorttable.js:1.5
    --------------------------------------------------------------------------------

--- src/WebChannel/main/webapp/xpedx/js/sorttable.js
+++ src/WebChannel/main/webapp/xpedx/js/sorttable.js
@@ -399,7 +399,8 @@ if (document.addEventListener) {
 /* for Internet Explorer */
 /*@cc_on @*/
 /*@if (@_win32)
-    document.write("<script id=__ie_onload defer src=javascript:void(0)><\/script>");
+	  document.write('<script id="__ie_onload" defer src="//:"><\/script>');
+
     var script = document.getElementById("__ie_onload");
     script.onreadystatechange = function() {
         if (this.readyState == "complete") {
