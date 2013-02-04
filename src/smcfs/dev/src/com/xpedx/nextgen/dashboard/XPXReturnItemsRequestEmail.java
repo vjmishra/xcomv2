1523fb8c96365997e920c2844569ebba48b08608
Mitesh Parikh <parikh.mitesh@hp.com>
2013-02-04 01:06:16 -0500

    XB-461 : Custom Email solution : Sterling's Email component has been removed now. Instead we will be calling the smtp server from our customized Java files. Status of the email will be stamped in the database [Table Name : XPX_EMAIL_DETAILS].
    ----------------------------------- StarTeam -----------------------------------
    A src/smcfs/dev/src/com/xpedx/nextgen/dashboard/XPXReturnItemsRequestEmail.java:1.0
    --------------------------------------------------------------------------------

---
+++ src/smcfs/dev/src/com/xpedx/nextgen/dashboard/XPXReturnItemsRequestEmail.java
@@ -0,0 +1,45 @@
+package com.xpedx.nextgen.dashboard;
+
+import java.util.Properties;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import com.sterlingcommerce.baseutil.SCXmlUtil;
+import com.xpedx.nextgen.common.util.XPXEmailUtil;
+import com.yantra.interop.japi.YIFCustomApi;
+import com.yantra.yfc.log.YFCLogCategory;
+import com.yantra.yfs.core.YFSSystem;
+import com.yantra.yfs.japi.YFSEnvironment;
+
+public class XPXReturnItemsRequestEmail implements YIFCustomApi {
+	private static YFCLogCategory yfcLog;
+
+	static {
+		yfcLog = (YFCLogCategory) YFCLogCategory.getLogger("com.xpedx.nextgen.log");
+	}
+	@Override
+	public void setProperties(Properties arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public Document sendReturnItemsRequestEmail(YFSEnvironment env, Document inXML)	throws Exception
+	{
+		String inputXML=SCXmlUtil.getString(inXML);
+		if(yfcLog.isDebugEnabled())
+			yfcLog.debug("Begin - Inside sendReturnItemsRequestEmail method of XPXReturnItemsRequestEmail. Input XML : "+ inputXML);
+		
+		Element rtrnItemsReqEmailElem=inXML.getDocumentElement();
+		String emailType=XPXEmailUtil.RETURN_ITEMS_REQUEST_EMAIL_TYPE;
+        String emailFrom=SCXmlUtil.getXpathAttribute(rtrnItemsReqEmailElem, "/Emails/Email/@FromEmail");
+        String emailSubject = XPXEmailUtil.RETURN_ITEMS_REQUEST_EMAIL_SUBJECT;
+        XPXEmailUtil.insertEmailDetailsIntoDB(env, inputXML, emailType, emailSubject, emailFrom, "");
+		
+		if(yfcLog.isDebugEnabled())
+			yfcLog.debug("End - Inside sendReturnItemsRequestEmail method of XPXReturnItemsRequestEmail.");
+		
+		return inXML;
+	}
+
+}
