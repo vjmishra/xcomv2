97ebd3da3001616f39bf17d723c67353e1e39dcc
Kubra Khan <kubra.khan@hp.com>
2013-05-07 15:47:37 +0530

    Modified for XB-519 Manage rule tab UI changes

--- src/smcfs/dev/PCAExtensions/plugins/com.xpedx.sterling.rcp.pca/com/xpedx/sterling/rcp/pca/customerprofilerule/screen/CustomerProfileRuleLinePanel.java
+++ src/smcfs/dev/PCAExtensions/plugins/com.xpedx.sterling.rcp.pca/com/xpedx/sterling/rcp/pca/customerprofilerule/screen/CustomerProfileRuleLinePanel.java
@@ -16,7 +16,6 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.w3c.dom.Element;
-
 import com.xpedx.sterling.rcp.pca.util.XPXConstants;
 import com.xpedx.sterling.rcp.pca.util.XPXUtils;
 import com.yantra.yfc.rcp.IYRCComposite;
@@ -63,7 +62,7 @@ public class CustomerProfileRuleLinePanel extends Composite implements IYRCCompo
 	private String ruleId;
 	private boolean showParam1=false;
 	private boolean showtext=false;
-	private boolean enablechk=false;
+	boolean enablechk=false;
 	private boolean showParam2=false;
 	private boolean showParam3=false;
 	private StyledText stxtRuleDesc;
@@ -110,6 +109,7 @@ public class CustomerProfileRuleLinePanel extends Composite implements IYRCCompo
 		initialize();
 		setBindingForComponents();
 		myBehavior = new CustomerProfileRuleLineBehavior(this, FORM_ID, inputObject, eleRuleLine);
+		
 		populateCustomerLineFields(this.pnlRuleLines.parentObj);
 	}
 	
@@ -231,6 +231,7 @@ public class CustomerProfileRuleLinePanel extends Composite implements IYRCCompo
 	}
 	private void setBindingForComponents() {
 		YRCButtonBindingData chkBoxBindingData = new YRCButtonBindingData();
+		
 		chkBoxBindingData.setCheckedBinding("Y");
 		chkBoxBindingData.setUnCheckedBinding("N");
 		chkBoxBindingData.setSourceBinding("Source:/XPXRuleDefn/@Selected");
@@ -243,6 +244,7 @@ public class CustomerProfileRuleLinePanel extends Composite implements IYRCCompo
 		stbd.setName("stxtRuleDesc");
 		stxtRuleDesc.setData(YRCConstants.YRC_STYLED_TEXT_BINDING_DEFINATION, stbd);
 		YRCTextBindingData tbd =null;
+		
 		if (showParam1) {
 			tbd = new YRCTextBindingData();
 			tbd.setSourceBinding("Source:/XPXRuleDefn/@Param1");
@@ -401,13 +403,14 @@ public class CustomerProfileRuleLinePanel extends Composite implements IYRCCompo
 	}
 	private void createParam1Composite() {
 
-		GridLayout gridLayout1 = new GridLayout();
-		gridLayout1.horizontalSpacing = 0;
-		gridLayout1.verticalSpacing = 0;
+		//XB-519 Modified For UI
+		GridLayout gridLayout1 = new GridLayout(6, true);
+		gridLayout1.horizontalSpacing = 2;
+		gridLayout1.verticalSpacing = 2;
 		//gridLayout1.numColumns = 1;
-		gridLayout1.numColumns = 2;
-		gridLayout1.marginWidth = 0;
-		gridLayout1.marginHeight = 5;
+		gridLayout1.numColumns = 6;
+		gridLayout1.marginWidth = 2;
+		gridLayout1.marginHeight = 2;
 		
 		//XB-519 Modified for UI
 		/*GridData gridDataVal = new GridData();
@@ -468,17 +471,7 @@ public class CustomerProfileRuleLinePanel extends Composite implements IYRCCompo
 		txtParam1.setLayoutData(gridDataVal);
 		txtParam1.setText("");
 		txtParam1.setData("name", "txtParam1");
-		if(showParam1){
-			txtParam1.setVisible(true);
-		}else{
-			txtParam1.setVisible(false);
-		}
-		if(enablechk){
-			txtParam1.setEditable(true);
-		}
-		else{
-			txtParam1.setEditable(false);
-		}
+	
 //		if(isIntegAdmin()){
 //			txtParam1.setEditable(false);
 //		}
@@ -522,7 +515,7 @@ public class CustomerProfileRuleLinePanel extends Composite implements IYRCCompo
 		}else{
 			txtParam2.setVisible(false);
 		}
-
+		
 	}
 	private void createParam3Composite() {
 
@@ -537,9 +530,10 @@ public class CustomerProfileRuleLinePanel extends Composite implements IYRCCompo
 		GridData gridData3 = new GridData();
 		gridData3.horizontalAlignment = SWT.BEGINNING;
 		gridData3.grabExcessHorizontalSpace = true;
-		gridData3.verticalAlignment = 2;
-		gridData3.widthHint = 300;
-		gridData3.horizontalSpan=3;
+		gridData3.verticalAlignment = 6;
+		gridData3.widthHint = 600;
+		gridData3.horizontalSpan=4;
+		
 		
 		//XB-519 - TextField UI On Rules TAB
 		GridData gridDataVal = new GridData();
@@ -589,6 +583,17 @@ public class CustomerProfileRuleLinePanel extends Composite implements IYRCCompo
 			txtParamRule.setVisible(false);
 			txtParamRule.setEditable(false);
 			
+		}
+		if(showParam1){
+			txtParam1.setVisible(true);
+		}else{
+			txtParam1.setVisible(false);
+		}
+		if(enablechk){
+			txtParam1.setEditable(true);
+		}
+		else{
+			txtParam1.setEditable(false);
 		}
 	}
 	
