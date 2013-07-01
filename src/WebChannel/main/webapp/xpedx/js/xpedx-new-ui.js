ccd8bc8fc7bc2f69fb996462e6fd70436c147a19
Ramya UR <ramya.ur@hp.com>
2013-07-01 16:13:49 +0530

    updated for EB 632 on Behalf of Shilpa Nr

--- src/WebChannel/main/webapp/xpedx/js/xpedx-new-ui.js
+++ src/WebChannel/main/webapp/xpedx/js/xpedx-new-ui.js
@@ -677,8 +677,8 @@ if(document.getElementById("calcWaste").value=="")
 function calcPhone(AddingText,objTxtBox)
 {
     y=objTxtBox.value;
-    abc=/-/gi;
-    rep=y.replace(abc,0)
+    abc=/^\.|-/gi;
+    rep=y.replace(abc,"0.");
     if (isNaN(rep))
     {
         ab=/(a*)(b*)(c*)(d*)(e*)(f*)(g*)(h*)(i*)(j*)(k*)(l*)(m*)(n*)(o*)(p*)(q*)(r*)(s*)(t*)(u*)(v*)(w*)(x*)(y*)(z*)/gi;
