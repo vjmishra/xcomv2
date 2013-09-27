7e7decf11be096357517b3b70f387e178e0d0e54
Sumit Rangdal <sumit.rangdal@hp.com>
2013-09-27 15:48:01 -0400

    Cerated Build file

--- src/ReportsApp/src/main/java/main/ReportWebServicePublisher.java
+++ src/ReportsApp/src/main/java/main/ReportWebServicePublisher.java
@@ -1,15 +1,34 @@
 package main;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.xml.ws.Endpoint;
 
 import com.reports.service.ReportServiceImpl;
 
 public class ReportWebServicePublisher {
-	
-	public static void main(String[] args) {
-		
-		Endpoint.publish("http://localhost:9994/ws/ReportService",new ReportServiceImpl());		 
+
+	private String getUrl() {
+		// Bring the value from properties file
+		Properties properties = new Properties();
+		InputStream is = this.getClass().getClassLoader()
+				.getResourceAsStream("reports_app.properties");
+		try {
+			properties.load(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return properties.getProperty("deployedUrl");
 	}
 
+	public static void main(String[] args) {
+
+		ReportWebServicePublisher p = new ReportWebServicePublisher();
+		String urlLocation = p.getUrl();
+		Endpoint.publish(urlLocation, new ReportServiceImpl());
+	}
 
 }
