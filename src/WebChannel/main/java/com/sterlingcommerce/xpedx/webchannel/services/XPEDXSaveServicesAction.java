73349bddb691752cbe1fd9c626147e4c337db8b2
Amar Jyoti Kumar <amar-jyoti.kumar@hp.com>
2014-04-15 13:46:59 +0530

    Code Merge EB-4800 from 13.01 to development for send mail even when  to email id doesnot exist

--- src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/services/XPEDXSaveServicesAction.java
+++ src/WebChannel/main/java/com/sterlingcommerce/xpedx/webchannel/services/XPEDXSaveServicesAction.java
@@ -565,14 +565,15 @@ public class XPEDXSaveServicesAction extends WCMashupAction {
 			 * If toEmail is empty then ccEamil should be toEmail .
 			 * 
 			 * */
-			if((toEmailGen == null || toEmailGen.trim().length() > 0) && (toEmailPaper == null || toEmailPaper.trim().length() > 0) && (ccEMail!=null && ccEMail.trim().length() > 0)){
-				if((toEmailGen == null || toEmailGen.trim().length() > 0) && (paperFSObj.equalsIgnoreCase("facilitySupplies"))){
-					toEmailGen = ccEMail;
-				}
-				if((toEmailPaper == null || toEmailPaper.trim().length() > 0) && (paperPSObj.equalsIgnoreCase("PaperSupplies"))){
-					toEmailPaper = ccEMail;
-				}
-				ccEMail = "";
+            if(((toEmailGen == null || toEmailGen.trim().length() == 0) || (toEmailPaper == null || toEmailPaper.trim().length() == 0)) && (ccEMail!=null && ccEMail.trim().length() > 0)){
+                if((toEmailGen == null || toEmailGen.trim().length() == 0) && (paperFSObj.equalsIgnoreCase("facilitySupplies"))){
+                    toEmailGen = ccEMail;
+                    ccEMail = "";
+                }
+                if((toEmailPaper == null || toEmailPaper.trim().length() == 0) && (paperPSObj.equalsIgnoreCase("PaperSupplies"))){
+                    toEmailPaper = ccEMail;
+                    ccEMail = "";
+                }
 			}else
 			if((toEmailGen == null || toEmailGen.trim().length() > 0) && (toEmailPaper != null && toEmailPaper.trim().length() > 0) && (ccEMail!=null && ccEMail.trim().length() > 0)){
 				if(paperFSObj.equalsIgnoreCase("facilitySupplies")){
