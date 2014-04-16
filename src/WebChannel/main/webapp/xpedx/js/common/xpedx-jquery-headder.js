72d1f4e1f4e8595181be4a62f6640a8680f9e8c6
Trey Howard <trey.howard@ipaper.com>
2014-04-16 09:47:46 -0400

    eb-5461: removed dead code, xpedx-jquery-headder.js references to arrow gifs

--- src/WebChannel/main/webapp/xpedx/js/common/xpedx-jquery-headder.js
+++ src/WebChannel/main/webapp/xpedx/js/common/xpedx-jquery-headder.js
@@ -2270,7 +2270,7 @@ var disappeardelay=250  //tooltip disappear delay (in miliseconds)
 var verticaloffset=-130 //added vertical offset of tooltip from anchor link, if any
 var horizontaloffset=-150 //added horizontal offset of tooltip from anchor link, if any
 var enablearrowhead=0 //0, 1, or 2 - to disable(0) or enable the top(1) or side(2) arrow image
-var arrowheadimg=["/swc/xpedx/images/icons/arrowdown.gif", "/swc/xpedx/images/icons/arrowup.gif", "/swc/xpedx/images/icons/arrowleft.gif", "/swc/xpedx/images/icons/arrowright.gif"] //path to down and up left and right arrow images
+var arrowheadimg=[]
 var arrowheadsize=11 //size of arrow image (amount to reveal)
 
 /////No further editting needed
