ff30de1b503534f5e9d62ffe3e41857e5504613c
Vijaya Chilukuri <vijaya.chilukuri@hp.com>
2014-05-23 16:05:21 -0400

    EB-6170 change the search feature in Change/select Ship-To pages/modal
    for performance issue

--- src/WebChannel/main/webapp/swc/js/common/ShipToComponent.js
+++ src/WebChannel/main/webapp/swc/js/common/ShipToComponent.js
@@ -23,7 +23,12 @@ function showShiptos(titleOfModal, customerContactId, getAssignedShipToURL, incl
 		Ext.Msg.hide();
 		myMask.hide();
 	}
-	
+
+	function reset() {
+		$('#ship-container').get(0).innerHTML = '';
+	}
+
+	reset();
 	showProcessingBar();
 	var url = getAssignedShipToURL;
 	$.ajax({
@@ -36,8 +41,8 @@ function showShiptos(titleOfModal, customerContactId, getAssignedShipToURL, incl
 			,'includeShoppingForAndDefaultShipTo':includeShoppingForAndDefaultShipTo
 		},
 		success : function(data) {
-			if (data.shipToList) {
-				var shipToList = data.shipToList;
+
+			if (data.shipToList) {	
 				var defaultShipToCustomer;
 				var defaultShipToAddressList;
 				var shoppingForShipToCustomer;
@@ -48,7 +53,7 @@ function showShiptos(titleOfModal, customerContactId, getAssignedShipToURL, incl
 					html.push('		<h2>',	titleOfModal,	'</h2>');
 				}
 				html.push('			<input type="hidden" name="userIdForSelectOrChangeShipTo" id="userIdForSelectOrChangeShipTo" value="', customerContactId, '"/>');
-				
+
 				if(includeShoppingForAndDefaultShipTo=='true'){
 					if (data.defaultShipToCustomer) {
 						defaultShipToCustomer = data.defaultShipToCustomer;
@@ -61,7 +66,7 @@ function showShiptos(titleOfModal, customerContactId, getAssignedShipToURL, incl
 					if (data.shoppingForShipToCustomer && data.defaultShipToCustomer) {
 						html.push('		 <div class="ship-current-wrap">');					
 						html.push('			<h3>Currently Shopping for:</h3>');
-					
+
 						html.push('			 <p>');	
 						html.push(					shoppingForShipToCustomer.customerID.replace("-M-XX-S",""), '<br/>');
 						html.push(					shoppingForShipToCustomer.organizationName,'<br/>');
@@ -73,10 +78,10 @@ function showShiptos(titleOfModal, customerContactId, getAssignedShipToURL, incl
 								html.push(					shoppingForAddressList[i], '<br/>');
 							}
 						}					
-							html.push(				shoppingForShipToCustomer.city, ', ', shoppingForShipToCustomer.state, ' ', shoppingForShipToCustomer.zipCode, ' ', shoppingForShipToCustomer.country, '<br/>');
-							html.push('			</p>');
+						html.push(				shoppingForShipToCustomer.city, ', ', shoppingForShipToCustomer.state, ' ', shoppingForShipToCustomer.zipCode, ' ', shoppingForShipToCustomer.country, '<br/>');
+						html.push('			</p>');
 						html.push('		 </div>');
-					
+
 						html.push('		 <div class="ship-preferred-wrap">');
 						html.push('			<h3>Preferred Ship-To:</h3>');
 						if (data.defaultShipToCustomer) {
@@ -104,12 +109,12 @@ function showShiptos(titleOfModal, customerContactId, getAssignedShipToURL, incl
 						html.push('		     </div>');						
 						html.push('		 </div>');
 					}else{
-							html.push('			 <div class="preferred-select" style="display:none">');
-							html.push('		     	<input type="checkbox" name="setAsDefault" class="change-preferred-ship-to" id="setAsDefault" checked');
-							html.push('				/>')
-							html.push('		     	<div>Change Preferred Ship-To to Selected</div>');
-							html.push('		     </div>');
-						}					
+						html.push('			 <div class="preferred-select" style="display:none">');
+						html.push('		     	<input type="checkbox" name="setAsDefault" class="change-preferred-ship-to" id="setAsDefault" checked');
+						html.push('				/>')
+						html.push('		     	<div>Change Preferred Ship-To to Selected</div>');
+						html.push('		     </div>');
+					}					
 				}
 				html.push('		    <div class="clearfix"></div>');	
 				html.push('			<hr class="divider-line addmargintop10">');
@@ -125,53 +130,39 @@ function showShiptos(titleOfModal, customerContactId, getAssignedShipToURL, incl
 				html.push('				<div class="clearfix"></div>');	
 				html.push('				<div class="ship-button-wrap">');				
 				if (applyShipToChanges && typeof(applyShipToChanges) === "function") { 				
-						html.push('				<input class="btn-gradient floatright" type="submit" value="Apply" name="applyShipToButton"/>');			
+					html.push('				<input class="btn-gradient floatright" type="submit" value="Apply" name="applyShipToButton"/>');			
 				}
 				if (selectShipToChanges && typeof(selectShipToChanges) === "function") {  
-						html.push('				<input class="btn-gradient floatright" type="submit" value="Select" name="selectShipToButton"/>');
+					html.push('				<input class="btn-gradient floatright" type="submit" value="Select" name="selectShipToButton"/>');
 				}
 				if (cancelShipToChanges && typeof(cancelShipToChanges) === "function") { 
-						html.push('				<input type="submit" value="Cancel" class="btn-neutral floatright margin-right-10" name="cancelShipToButton" />');
+					html.push('				<input type="submit" value="Cancel" class="btn-neutral floatright margin-right-10" name="cancelShipToButton" />');
 				}
 				html.push('				</div>');
-				
 
-				html.push('				<div id="header" class="ship-search-input-wrap"></div>')				
-				html.push('					<ul id="list">');
-				for (var k = 0, len = shipToList.length; k < len; k++) {
-					var shipTo = shipToList[k];
-					var shipToCustomerId = shipTo.shipToCustomerID;
-					var selectedShipToID = "selectedShipTo_"+shipToCustomerId; 
-					var shipToMultiRowDisplayID = "shipToMultiRowDisplay_"+shipToCustomerId;
-					html.push('					<li>');
-					html.push('						<a class="underlink;">');
-					html.push('							<input id="', selectedShipToID, '" type="radio" value="', shipTo.shipToCustomerID, '" name="selectedShipTo" />');
-					html.push('							<label for="', selectedShipToID, '">', shipTo.shipToDisplayString, '</label>');
-					html.push('						</a>');
-					html.push('						<input type="hidden" name="shipToMultiRowDisplay" id="',shipToMultiRowDisplayID,'" value="',shipTo.shipToMultiRowDisplay,'"/>');
-					html.push('					</li>');
-				}
-				html.push('					</ul>');
-				html.push('					<hr class="divider-line addmargintop10"/>');
 
-				html.push('					<div class="ship-button-wrap">');
+				html.push('				<div id="header" class="ship-search-input-wrap"></div>');
+				html.push('				<div id="ship-list-wrap"></div>');
+				html.push('				<hr class="divider-line addmargintop10"/>');
+
+				html.push('				<div class="ship-button-wrap">');
 
 				if (applyShipToChanges && typeof(applyShipToChanges) === "function") {					
-						html.push('					<input class="btn-gradient floatright" type="submit" value="Apply" name="applyShipToButton"/>');
+					html.push('					<input class="btn-gradient floatright" type="submit" value="Apply" name="applyShipToButton"/>');
 				}
 				if (selectShipToChanges && typeof(selectShipToChanges) === "function") {  
-						html.push('					<input class="btn-gradient floatright" type="submit" value="Select" name="selectShipToButton"/>');
+					html.push('					<input class="btn-gradient floatright" type="submit" value="Select" name="selectShipToButton"/>');
 				}
 				if (cancelShipToChanges && typeof(cancelShipToChanges) === "function") { 
-						html.push('					<input class="btn-neutral floatright margin-right-10" type="submit" value="Cancel" name="cancelShipToButton"/>');
+					html.push('					<input class="btn-neutral floatright margin-right-10" type="submit" value="Cancel" name="cancelShipToButton"/>');
 				}
 				html.push('					</div>');
 				html.push('					<div class="clearfix"></div>');
 				if (((!data.defaultShipToCustomer) || (data.defaultShipToCustomer && "30" !== data.defaultShipToCustomer.customerStatus)) && (!selectShipToChanges)) {					
-						html.push('			<div class="notice float-right addmargintop10 shipToErrTxt">Changing the Ship-To could impact pricing on orders.</div>');
+					html.push('			<div class="notice float-right addmargintop10 shipToErrTxt">Changing the Ship-To could impact pricing on orders.</div>');
 				}
 				else{
-						html.push('			<div class="float-right addmargintop10 shipToErrTxt"></div>');
+					html.push('			<div class="float-right addmargintop10 shipToErrTxt"></div>');
 				}
 				html.push('					<div class="clearfix"></div>');
 				html.push('				</div>');
@@ -188,44 +179,68 @@ function showShiptos(titleOfModal, customerContactId, getAssignedShipToURL, incl
 				if (cancelShipToChanges && typeof(cancelShipToChanges) === "function") {
 					$("input[name='cancelShipToButton']").click(cancelShipToChanges);
 				}
+
 				// create search input box in header div
-				var form = $("<form>").attr({"class":"filterform","action":"#"}),
-				input = $("<input>").attr({"class":"filterinput","type":"text"});
+				var form = $("<form>").attr({"class":"filterform", "action":"#"}),
+				input = $("<input>").attr({"class":"filterinput", "type":"text"});
 				$(form).append(input).appendTo($("#header"));
 
-				//create jquery expression ':Contains' for use with ship-to search plugin
-				jQuery.expr[':'].Contains = function(a,i,m){
-					return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
-				};
-
-				// this will filter the data to hide/show ship-to's based on the user input on search input box.
-				$(input)
-				.change( function () {
-					var filter = $(this).val();
-					if(filter) {
-						$('#list').find("a:not(:Contains(" + filter + "))").parent().slideUp();
-						$('#list').find("a:Contains(" + filter + ")").parent().slideDown();
-					} else {
-						$('#list').find("li").slideDown();
-					}
+				// re-create ship-to list when user performs filter operation
+				$('.filterform').submit(function() {
+					var filter = $(this).find('.filterinput').val();
+					displaySearchResults(filter);
 					return false;
-				})
-				.keyup( function () {
-					$(this).change();
 				});
-			} else {
-				
-				if (applyNoShioTo && typeof(applyNoShioTo) === "function") {
-					applyNoShioTo();
-				}
+
+				function displaySearchResults(filter) {
+					var shipToList = [];
+					if (filter) {							
+						for (var i = 0, len = data.shipToList.length; i < len; i++) {
+							var shipTo = data.shipToList[i];
+							if (shipTo.shipToDisplayString.toLowerCase().indexOf(filter.toLowerCase()) != -1) {
+								shipToList.push(shipTo);
+							}
+						}
+
+					} else {
+						// no filter, so use full list
+						shipToList = data.shipToList;
+					}
+					var shipToSelected=$("input[class='shipto-input']:checked").val();//ship To Selected before search
+
+
+					var $shipListWrapDiv = $("#ship-list-wrap");
+					var html = [];
+					html.push('					<ul id="list">');
+					for (var k = 0, len = shipToList.length; k < len; k++) {
+						var shipTo = shipToList[k];
+						var shipToCustomerId = shipTo.shipToCustomerID;
+						var selectedShipToID = "selectedShipTo_"+shipToCustomerId; 
+						var shipToMultiRowDisplayID = "shipToMultiRowDisplay_"+shipToCustomerId;
+						html.push('					<li>');
+						html.push('						<a class="underlink;">');
+						html.push('							<input id="', selectedShipToID, '" type="radio" class="shipto-input" ' , (shipToSelected == shipToCustomerId ? 'checked' : ''), ' value="', shipTo.shipToCustomerID, '" name="selectedShipTo" />');
+						html.push('							<label for="', selectedShipToID, '">', shipTo.shipToDisplayString, '</label>');
+						html.push('						</a>');
+						html.push('						<input type="hidden" name="shipToMultiRowDisplay" id="',shipToMultiRowDisplayID,'" value="',shipTo.shipToMultiRowDisplay,'"/>');
+						html.push('					</li>');
+					}
+					html.push('					</ul>');
+					$shipListWrapDiv.get(0).innerHTML = html.join('');
+				} // end displaySearchResults
+
+				displaySearchResults(''); //to show all ship-tos at the initial load
+
+			} else if (applyNoShioTo && typeof(applyNoShioTo) === "function") {
+				applyNoShioTo();
 			}
 		},
 		error: function(resp, textStatus, xhr) {
 			var $shipTosContainerDiv = $('#ship-container');
-				$shipTosContainerDiv.get(0).innerHTML = '<div class="error">Currently unable get assigned Ship-Tos. Please try again later</div>';
+			$shipTosContainerDiv.get(0).innerHTML = '<div class="error">Currently unable get assigned Ship-Tos. Please try again later</div>';
 			if (console) { console.log('showShiptos ajax error: resp = ', resp, '   textStatus = ', textStatus, '   xhr = ', xhr); }
-		}
-		,complete: function(){			
+		},
+		complete: function(){			
 			hideProcessingBar();
 		}
 	});
