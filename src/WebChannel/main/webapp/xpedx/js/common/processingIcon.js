66ced842bca9fddd00c737fce0f5b4b126a0d518
Shailendra Napit <shailendra.napit@hp.com>
2014-06-05 13:44:41 -0400

    EB-5612: Code format and different files.

--- src/WebChannel/main/webapp/xpedx/js/common/processingIcon.js
+++ src/WebChannel/main/webapp/xpedx/js/common/processingIcon.js
@@ -1,13 +1,12 @@
 /*-----------Processing Icon Methods------------*/
-
 function showProcessingIcon(){
-			$(".loading-wrap").css('display','block');
-			$(".loading-icon").css('display','block');
-			$("body").css("overflow", "hidden");
-		}
+	$(".loading-wrap").css('display','block');
+	$(".loading-icon").css('display','block');
+	$("body").css("overflow", "hidden");
+}
 
 function hideProcessingIcon(){
-			$(".loading-wrap").css('display','none');
-			$(".loading-icon").css('display','none');
-			$("body").css("overflow", "auto");
-		}
+	$(".loading-wrap").css('display','none');
+	$(".loading-icon").css('display','none');
+	$("body").css("overflow", "auto");
+}
