aeaedf70d674754e647dad72f44a5780f09df5d0
Vijay Mishra <vijay.mishra@hp.com>
2014-06-09 11:35:48 -0400

    EB-5955 - Fix the issue with Queue failure.

--- src/smcfs/dev/src/com/xpedx/nextgen/om/agent/email/ReprocessOrderConfirmationEmail.java
+++ src/smcfs/dev/src/com/xpedx/nextgen/om/agent/email/ReprocessOrderConfirmationEmail.java
@@ -121,15 +121,18 @@ public class ReprocessOrderConfirmationEmail extends YCPBaseAgent {
 				}
 				String cOrder=SCXmlUtil.getString(orderConfirmationEmailElem);
 				Document cOrderDoc=SCXmlUtil.createFromString(cOrder);
+				XPXUtils utilsObj=new XPXUtils();
 				try {
-					XPXUtils utilsObj=new XPXUtils();
+					orderEmailConfirmationSentFlag="Y";
 					updateEmailFlag = utilsObj.updateEmailSentFlag(env, cOrderHeaderKey, orderEmailConfirmationSentFlag, this.getClass().getSimpleName());
 					if(updateEmailFlag==true){
-						api.executeFlow(env, "XPXPutOrderChangesInOrderConfirmationEmailQueue", cOrderDoc);
-						orderEmailConfirmationSentFlag="Y";
+						api.executeFlow(env, "XPXPutOrderChangesInOrderConfirmationEmailQueue", cOrderDoc);						
 					}
 				}catch(Exception ex) {
 					log.error("Exception occured on posting order confirmation email XML to XPXPutOrderChangesInOrderConfirmationEmailQueue service "+ex.getMessage());
+					//EB-5955 - Duplicate Order Confirmation EMails -Code to verify if there is an issue with Queue mark Email sent flag as N.
+					orderEmailConfirmationSentFlag="N";
+					updateEmailFlag = utilsObj.updateEmailSentFlag(env, cOrderHeaderKey, orderEmailConfirmationSentFlag, this.getClass().getSimpleName());
 				}			
 			
 			} else if("OrderApproved".equals(lastOrderOperation)) {
