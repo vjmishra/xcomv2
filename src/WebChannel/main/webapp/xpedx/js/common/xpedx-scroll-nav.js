982c07e9b46546c1f89f0800d00cc956ee75c72e
Trey Howard <trey.howard@ipaper.com>
2014-07-18 14:29:38 -0400

    eb-6922: Reduced the cutoff width for scroll buttons to show them until scroll buttons would overlay content
    reduced width of salespro page to match site, to conform with scroll buttons cutoff dimensions

--- src/WebChannel/main/webapp/xpedx/js/common/xpedx-scroll-nav.js
+++ src/WebChannel/main/webapp/xpedx/js/common/xpedx-scroll-nav.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		
 		var $main = $('#main');
 		if ($main.length > 0) {
-			if (($main.offset().left + $main.width()) <= 1150) {
+			if (($main.offset().left + $main.width()) <= 1050) {
 				// hide scrollbars when content becomes narrow enough that scrollbars would overlay content
 				showScroll = false;
 			}
