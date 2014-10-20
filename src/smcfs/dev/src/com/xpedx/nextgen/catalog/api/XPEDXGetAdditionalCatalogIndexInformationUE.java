919f8149e03cddb3b51a353395844b9ac0c62012
Trey Howard <trey.howard@ipaper.com>
2014-10-20 09:47:25 -0400

    eb-8038: added proof of concept to filter out unentitled items from the lucene index

--- src/smcfs/dev/src/com/xpedx/nextgen/catalog/api/XPEDXGetAdditionalCatalogIndexInformationUE.java
+++ src/smcfs/dev/src/com/xpedx/nextgen/catalog/api/XPEDXGetAdditionalCatalogIndexInformationUE.java
@@ -4,12 +4,19 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.Writer;
 import java.rmi.RemoteException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -19,6 +26,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import com.sterlingcommerce.baseutil.SCXmlUtil;
+import com.sterlingcommerce.woodstock.util.frame.Manager;
 import com.xpedx.nextgen.catalog.api.ItemIndexUtil.ItemMetadata;
 import com.yantra.interop.japi.YIFClientCreationException;
 import com.yantra.ycm.japi.ue.YCMGetAdditionalCatalogIndexInformationUE;
@@ -49,10 +57,13 @@ public class XPEDXGetAdditionalCatalogIndexInformationUE implements YCMGetAdditi
 			log.debug("inStockStatus = " + inStockStatus);
 		}
 
-		dumpInputXml(inDocumentUE);
+		dumpInputXml(inDocumentUE, "input");
+		removeUnentitledItems(inDocumentUE);
+		dumpInputXml(inDocumentUE, "filtered");
 
 		try {
 			YFCDocument inDoc = YFCDocument.getDocumentFor(inDocumentUE);
+
 			YFCElement inElem = inDoc.getDocumentElement();
 			YFCIterable<YFCElement> searchFieldListIterator = inElem.getChildElement("SearchIndexFieldList").getChildren("SearchField");
 			YFCIterable<YFCElement> itemListIterator = inElem.getChildElement("ItemList").getChildren("Item");
@@ -75,13 +86,15 @@ public class XPEDXGetAdditionalCatalogIndexInformationUE implements YCMGetAdditi
 	/**
 	 * Writes the input xml to a file in the temp directory. This method is guaranteed to not throw exceptions.
 	 */
-	private static void dumpInputXml(Document inDocumentUE) {
-		File xmlFile = new File(System.getProperty("java.io.tmpdir"), "XPEDXGetAdditionalCatalogIndexInformationUE-input_" + new Date().getTime() + ".xml");
+	private static void dumpInputXml(Document inDocumentUE, String label) {
+		String filename = "XPEDXGetAdditionalCatalogIndexInformationUE" + "_" + new Date().getTime() + "_" + label + ".xml";
+		File xmlFile = new File(System.getProperty("java.io.tmpdir"), filename);
 		System.out.println("Logging inDocumentUE to file: " + xmlFile);
 
 		try {
 			TransformerFactory tFactory = TransformerFactory.newInstance();
 			Transformer transformer = tFactory.newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 
 			DOMSource source = new DOMSource(inDocumentUE);
 			StreamResult result = new StreamResult(xmlFile);
@@ -93,6 +106,7 @@ public class XPEDXGetAdditionalCatalogIndexInformationUE implements YCMGetAdditi
 			try {
 				writer = new FileWriter(xmlFile);
 				writer.write("Failed to write inDocumentUE to this file. Exception.message: " + e.getMessage() + "\n");
+				writer.flush();
 
 			} catch (Exception ignore) {
 				// do nothing
@@ -167,10 +181,18 @@ public class XPEDXGetAdditionalCatalogIndexInformationUE implements YCMGetAdditi
 	}
 
 	// TODO integrate this with Jim's query for entitled items
-	private static void removeUnentitledItems(Document inDocumentUE, Set<String> entitledItemIDs) {
+	private static void removeUnentitledItems(Document inDocumentUE) {
 		Element searchIndexTriggerElem = inDocumentUE.getDocumentElement();
 		Element itemListElem = SCXmlUtil.getChildElement(searchIndexTriggerElem, "ItemList");
 		List<Element> itemElems = SCXmlUtil.getElements(itemListElem, "Item");
+
+		Set<String> allItemIDs = new LinkedHashSet<String>(itemElems.size());
+		for (Element itemElem : itemElems) {
+			String itemID = itemElem.getAttribute("ItemID");
+			allItemIDs.add(itemID);
+		}
+
+		Set<String> entitledItemIDs = pocGetEntitledItemIDs(allItemIDs);
 
 		// remove the unentitled items from the dom
 		for (Element itemElem : itemElems) {
@@ -180,7 +202,64 @@ public class XPEDXGetAdditionalCatalogIndexInformationUE implements YCMGetAdditi
 			}
 		}
 
-		System.out.println(SCXmlUtil.getElements(itemListElem, "Item").size());
+//		System.out.println(SCXmlUtil.getElements(itemListElem, "Item").size());
+	}
+
+	private static Set<String> pocGetEntitledItemIDs(Set<String> allItemIDs) {
+		// TODO if this works, will refactor into an API call of some sort
+		//      guaranteed to not throw any exceptions since this query will fail in non-dev environments
+		Connection conn = null;
+		try {
+			conn = getConnection();
+
+			PreparedStatement stmt = conn.prepareStatement("select item_id from trey_entitled_item where item_id in (" + createQuestionMarks(allItemIDs.size()) + ")");
+
+			int parameterIndex = 1;
+			for (String itemID : allItemIDs) {
+				stmt.setString(parameterIndex++, itemID);
+			}
+
+			ResultSet res = stmt.executeQuery();
+
+			Set<String> entitledItemIDs = new LinkedHashSet<String>();
+			while (res.next()) {
+				entitledItemIDs.add(res.getString("item_id"));
+			}
+
+			System.out.println("Entitlement query succeeded: " + entitledItemIDs.size() + " / " + allItemIDs.size() + " are entitled items");
+
+			return entitledItemIDs;
+
+		} catch (Exception ignore) {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (Exception ignore2) {
+				}
+			}
+			return allItemIDs;
+		}
+	}
+
+	private static String createQuestionMarks(int num) {
+		StringBuilder buf = new StringBuilder(2 * num);
+		for (int i = 0; i < num; i++) {
+			if (i > 0) {
+				buf.append(",");
+			}
+			buf.append("?");
+		}
+		return buf.toString();
+	}
+
+	private static Connection getConnection() throws ClassNotFoundException, SQLException {
+		String jdbcDriver = Manager.getProperty("jdbcService", "oraclePool.driver");
+		String jdbcURL = Manager.getProperty("jdbcService", "oraclePool.url");
+		String jdbcUser = Manager.getProperty("jdbcService", "oraclePool.user");
+		String jdbcPassword = Manager.getProperty("jdbcService", "oraclePool.password");
+
+		Class.forName(jdbcDriver);
+		return DriverManager.getConnection(jdbcURL, jdbcUser, jdbcPassword);
 	}
 
 //	public static void main(String[] args) throws Exception {
