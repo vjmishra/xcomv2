bc90880b3ff3beb63e0e4e85579894d7a4a2b437
Vijaya Chilukuri <vijaya.chilukuri@hp.com>
2014-11-05 17:50:19 -0500

    EB-8346. Added print statements for debugging the split orders update through max order update. planning remove print statements once issue resolved

--- src/smcfs/dev/src/com/xpedx/nextgen/order/XPXOrderUpdateUtils.java
+++ src/smcfs/dev/src/com/xpedx/nextgen/order/XPXOrderUpdateUtils.java
@@ -8,6 +8,7 @@ import org.w3c.dom.Element;
 import com.sterlingcommerce.baseutil.SCUtil;
 import com.sterlingcommerce.baseutil.SCXmlUtil;
 import com.sterlingcommerce.tools.datavalidator.XmlUtils;
+import com.yantra.yfc.util.YFCCommon;
 
 public class XPXOrderUpdateUtils {
 
@@ -18,34 +19,41 @@ public class XPXOrderUpdateUtils {
 	 */
 	public static void setBackorderQtyFlagIfExists(Document outputDoc){
 		String reqBackOrdQtyExist = "N";
-		if(outputDoc != null && outputDoc.getDocumentElement() != null && !"Y".equalsIgnoreCase(outputDoc.getDocumentElement().getAttribute("DraftOrderFlag"))){
-			Element outputDocElement = outputDoc.getDocumentElement();
-			ArrayList<Element> orderLineElemList = SCXmlUtil.getElements(outputDocElement,"OrderLines/OrderLine");
-			if(orderLineElemList != null && orderLineElemList.size() > 0){
-				for (int i = 0; i < orderLineElemList.size(); i++) {
-					Element orderLine = orderLineElemList.get(i);
-					if (orderLine != null && orderLine.getAttribute("LineType") != "C" && orderLine.getAttribute("LineType") != "M"){
-						Element orderLineExtnElem = XmlUtils.getChildElement(orderLine, "Extn");
-						if(orderLineExtnElem != null && !SCUtil.isVoid(orderLineExtnElem.getAttribute("ExtnReqBackOrdQty"))){
-							double backOrderedQty = 0;
-							try {
-								backOrderedQty = Double.parseDouble(orderLineExtnElem.getAttribute("ExtnReqBackOrdQty"));
-							} catch (NumberFormatException e) {
-								e.printStackTrace();
-							}
-							if(backOrderedQty > 0) {
-								reqBackOrdQtyExist = "Y";
-								break;
+		if(outputDoc != null && outputDoc.getDocumentElement() != null){
+			System.out.println("outputDoc.getDocumentElement():" + outputDoc.getDocumentElement());
+			System.out.println("Draft order flag:" + outputDoc.getDocumentElement().getAttribute("DraftOrderFlag"));
+			String draftOrderFlag = YFCCommon.isVoid(outputDoc.getDocumentElement().getAttribute("DraftOrderFlag")) ? "N":outputDoc.getDocumentElement().getAttribute("DraftOrderFlag").trim();
+			System.out.println("draftOrderFlag :" + draftOrderFlag);
+			if(!"Y".equalsIgnoreCase(draftOrderFlag)){
+				Element outputDocElement = outputDoc.getDocumentElement();
+				ArrayList<Element> orderLineElemList = SCXmlUtil.getElements(outputDocElement,"OrderLines/OrderLine");
+				if(orderLineElemList != null && orderLineElemList.size() > 0){
+					for (int i = 0; i < orderLineElemList.size(); i++) {
+						Element orderLine = orderLineElemList.get(i);
+						if (orderLine != null && orderLine.getAttribute("LineType") != "C" && orderLine.getAttribute("LineType") != "M"){
+							Element orderLineExtnElem = XmlUtils.getChildElement(orderLine, "Extn");
+							if(orderLineExtnElem != null && !SCUtil.isVoid(orderLineExtnElem.getAttribute("ExtnReqBackOrdQty"))){
+								double backOrderedQty = 0;
+								try {
+									backOrderedQty = Double.parseDouble(orderLineExtnElem.getAttribute("ExtnReqBackOrdQty"));
+								} catch (NumberFormatException e) {
+									e.printStackTrace();
+								}
+								if(backOrderedQty > 0) {
+									reqBackOrdQtyExist = "Y";
+									break;
+								}
 							}
 						}
 					}
 				}
+				Element orderExtnElem = XmlUtils.getChildElement(outputDocElement, "Extn");
+				if(orderExtnElem == null){
+					orderExtnElem = XmlUtils.createChild(outputDocElement, "Extn");
+				}
+				orderExtnElem.setAttribute("ExtnReqBackOrdQtyExist", reqBackOrdQtyExist);
+				System.out.println("after setting flag outputDoc.getDocumentElement():" + outputDoc.getDocumentElement());
 			}
-			Element orderExtnElem = XmlUtils.getChildElement(outputDocElement, "Extn");
-			if(orderExtnElem == null){
-				orderExtnElem = XmlUtils.createChild(outputDocElement, "Extn");
-			}
-			orderExtnElem.setAttribute("ExtnReqBackOrdQtyExist", reqBackOrdQtyExist);
 		}
 	}
 }
